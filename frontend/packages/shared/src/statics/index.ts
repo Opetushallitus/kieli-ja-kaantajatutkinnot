a7a8ccae58c9fd7ee4c4e920614e574374c2707d
Pejam Hassanzadeh <pejam.hassanzadeh@knowit.fi>
2022-05-11 15:13:18 +0300

    SHARED(Frontend): Export static files

---
+++ frontend/packages/shared/src/statics/index.ts
@@ -0,0 +1,13 @@
+import FooterWave from './svg/footer_wave.svg';
+import OPHLogoENHorizontal from './svg/oph_logo_horiz_en.svg';
+import OPHLogoHorizontal from './svg/oph_logo_horiz_fi_sv.svg';
+import OPHLogoENVertical from './svg/oph_logo_vert_en.svg';
+import OPHLogoVertical from './svg/oph_logo_vert_fi_sv.svg';
+
+export {
+  FooterWave,
+  OPHLogoENHorizontal,
+  OPHLogoHorizontal,
+  OPHLogoENVertical,
+  OPHLogoVertical,
+};
