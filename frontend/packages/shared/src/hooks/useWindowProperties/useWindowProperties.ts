d2d95da74692b08427d6929d698e1b418f650e5a
Pyry Koivisto <pyry.koivisto@iki.fi>
2022-05-12 16:45:50 +0300

    SHARED(Frontend): Move more hooks under shared package

---
+++ frontend/packages/shared/src/hooks/useWindowProperties/useWindowProperties.ts
@@ -0,0 +1,33 @@
+import { useEffect, useState } from 'react';
+import { Screenwidth } from 'shared/enums';
+
+const getProperties = () => {
+  const { innerWidth: width, innerHeight: height } = window;
+  const isPhone = width <= Screenwidth.Phone;
+  const isTablet = width >= Screenwidth.Tablet && width < Screenwidth.Desktop;
+  const isDesktop = width >= Screenwidth.Desktop;
+
+  return {
+    isPhone,
+    isTablet,
+    isDesktop,
+    width,
+    height,
+  };
+};
+
+export const useWindowProperties = () => {
+  const [windowDimensions, setWindowDimensions] = useState(getProperties());
+
+  useEffect(() => {
+    const resizeEvent = 'resize';
+    const handleResize = () => {
+      setWindowDimensions(getProperties());
+    };
+    addEventListener(resizeEvent, handleResize);
+
+    return () => removeEventListener(resizeEvent, handleResize);
+  }, []);
+
+  return windowDimensions;
+};
