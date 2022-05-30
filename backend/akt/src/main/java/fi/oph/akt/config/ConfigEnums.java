3b156fe5a250e25f62bbce039c48caa94dee9bdc
Mika Huttunen <mika.huttunen@gofore.com>
2022-05-30 15:27:05 +0300

    AKT(Backend): OPHAKTKEH-360 usage of ConfigEnums

--- backend/akt/src/main/java/fi/oph/akt/config/ConfigEnums.java
+++ backend/akt/src/main/java/fi/oph/akt/config/ConfigEnums.java
@@ -2,6 +2,7 @@ package fi.oph.akt.config;
 
 public enum ConfigEnums {
   CALLER_ID("1.2.246.562.10.00000000001.akt"),
+  EMAIL_SENDER_NAME("AKR"),
   SERVICENAME("akt");
 
   private final String value;
