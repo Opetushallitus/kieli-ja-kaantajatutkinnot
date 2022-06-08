7306ebd79fdbb80bb520eac42b3df5b2dd2fcc27
Pejam Hassanzadeh <pejam.hassanzadeh@knowit.fi>
2022-06-08 10:04:57 +0300

    AKT(Frontend): Fix docker issua and refactor webpack common

--- frontend/packages/akt/webpack.config.js
+++ frontend/packages/akt/webpack.config.js
@@ -2,27 +2,7 @@ const { merge } = require('webpack-merge');
 const common = require('../../webpack.common');
 
 module.exports = (env) => {
-  const {
-    getMode,
-    getEntry,
-    getOutput,
-    getPlugins,
-    getModules,
-    getDevTool,
-    getResolve,
-    getStats,
-    getDevServer,
-  } = common('akt', env, __dirname);
+  const { getDefaults } = common('akt', env, __dirname);
 
-  return merge([
-    getMode(),
-    getEntry(),
-    getOutput(),
-    getPlugins(),
-    getModules(),
-    getResolve(),
-    getDevTool(),
-    getStats(),
-    getDevServer(),
-  ]);
+  return merge([getDefaults()]);
 };
