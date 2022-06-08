10102c95707743209f5b961ec08c3b47e62d676a
Pejam Hassanzadeh <pejam.hassanzadeh@knowit.fi>
2022-06-08 12:25:41 +0300

    SHARED(Frióntend): Use common webpack

--- frontend/packages/shared/webpack.config.js
+++ frontend/packages/shared/webpack.config.js
@@ -1,65 +1,32 @@
-const path = require('path');
-const webpack = require('webpack');
+const { merge } = require('webpack-merge');
 const ESLintPlugin = require('eslint-webpack-plugin');
-const CompressionPlugin = require('compression-webpack-plugin');
+const StylelintPlugin = require('stylelint-webpack-plugin');
+
+const common = require('../../webpack.common');
 
 module.exports = (env) => {
-  const STATIC_PATH = 'shared/static';
-  const mode = env.prod ? 'production' : 'development';
+  const { getMode, getEntry, getModules } = common('shared', env, __dirname);
 
-  return {
-    mode,
-    entry: path.join(__dirname, 'src', 'index.tsx'),
-    plugins: [
-      new CompressionPlugin({
-        algorithm: 'gzip',
-      }),
-      new ESLintPlugin({
-        extensions: ['ts', 'tsx'],
-      }),
-      new webpack.DefinePlugin({
-        REACT_ENV_PRODUCTION: JSON.stringify(Boolean(env.prod)),
-      }),
-      ...getStylelintPlugin(env),
-    ],
-    module: {
-      rules: [
-        {
-          test: /\.[tj]sx?$/,
-          loader: 'babel-loader',
-          exclude: /node_modules\/(?!(shared)\/).*/,
-        },
-        {
-          test: /\.s?css$/,
-          use: ['style-loader', 'css-loader', 'sass-loader'],
-          exclude: /node_modules\/(?!(shared)\/).*/,
-        },
-        {
-          test: /\.svg$/,
-          type: 'asset/resource',
-          generator: {
-            filename: `${STATIC_PATH}/assets/svg/[name][ext]`,
-          },
-        },
+  return merge([
+    getMode(),
+    getEntry(),
+    getModules(),
+    {
+      plugins: [
+        new ESLintPlugin({
+          extensions: ['ts', 'tsx'],
+        }),
+        new StylelintPlugin(),
       ],
     },
-    resolve: {
-      extensions: ['.js', '.jsx', '.ts', '.tsx'],
-      modules: [path.resolve(__dirname, 'src'), 'node_modules'],
-      alias: {
-        public: path.resolve(__dirname, 'public/'),
+    {
+      resolve: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+        alias: {
+          public: path.resolve(__dirname, 'public/'),
+        },
       },
     },
-    devtool: env.prod ? 'source-map' : 'cheap-module-source-map',
-    stats: 'errors-warnings',
-  };
-};
-
-// Helpers
-const getStylelintPlugin = (env) => {
-  if (!env.isCypress) {
-    const StylelintPlugin = require('stylelint-webpack-plugin');
-    return [new StylelintPlugin()];
-  }
-  return [];
+  ]);
 };
