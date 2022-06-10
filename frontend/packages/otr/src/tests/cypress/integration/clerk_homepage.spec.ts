a8c03c38b8e6ab381b85aea0ae5a8255d62515b2
Pyry Koivisto <pyry.koivisto@iki.fi>
2022-06-10 18:57:26 +0300

    OTR(Frontend): Test autocomplete filters on ClerkHomePage

--- frontend/packages/otr/src/tests/cypress/integration/clerk_homepage.spec.ts
+++ frontend/packages/otr/src/tests/cypress/integration/clerk_homepage.spec.ts
@@ -1,6 +1,7 @@
 import dayjs from 'dayjs';
 
 import { QualificationStatus } from 'enums/clerkInterpreter';
+import { ExaminationType } from 'enums/interpreter';
 import { onClerkHomePage } from 'tests/cypress/support/page-objects/clerkHomePage';
 import { useFixedDate } from 'tests/cypress/support/utils/date';
 import { clerkInterpreters10 } from 'tests/msw/fixtures/clerkInterpreters10';
@@ -10,19 +11,40 @@ const fixedDateForTests = dayjs('2022-06-10T16:00:00+0200');
 beforeEach(() => {
   cy.openClerkHomePage();
   useFixedDate(fixedDateForTests);
+  onClerkHomePage.expectFilteredInterpretersCount(clerkInterpreters10.length);
 });
 
 describe('ClerkHomePage', () => {
   it('should allow filtering interpreters by qualification status', () => {
-    onClerkHomePage.expectFilteredInterpretersCount(clerkInterpreters10.length);
-
     onClerkHomePage.filterByQualificationStatus(QualificationStatus.Expiring);
-    onClerkHomePage.expectFilteredInterpretersCount(0);
+    onClerkHomePage.expectFilteredInterpretersCount(1);
 
     onClerkHomePage.filterByQualificationStatus(QualificationStatus.Expired);
-    onClerkHomePage.expectFilteredInterpretersCount(0);
+    onClerkHomePage.expectFilteredInterpretersCount(1);
 
     onClerkHomePage.filterByQualificationStatus(QualificationStatus.Effective);
     onClerkHomePage.expectFilteredInterpretersCount(clerkInterpreters10.length);
+  });
+
+  it('should allow filtering interpreters on the basis of different qualification criteria', () => {
+    onClerkHomePage.filterByPermissionToPublish(false);
+    onClerkHomePage.expectFilteredInterpretersCount(1);
+
+    onClerkHomePage.filterByPermissionToPublish(true);
+    onClerkHomePage.expectFilteredInterpretersCount(9);
+
+    onClerkHomePage.filterByExaminationType(ExaminationType.Other);
+    onClerkHomePage.expectFilteredInterpretersCount(3);
+
+    onClerkHomePage.filterByExaminationType(
+      ExaminationType.LegalInterpreterExam
+    );
+    onClerkHomePage.expectFilteredInterpretersCount(6);
+
+    onClerkHomePage.filterByFromLanguage('suomi');
+    onClerkHomePage.expectFilteredInterpretersCount(6);
+
+    onClerkHomePage.filterByToLanguage('tanska');
+    onClerkHomePage.expectFilteredInterpretersCount(1);
   });
 });
