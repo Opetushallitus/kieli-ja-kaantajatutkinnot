be38b9b8c06dd167017bef7e95291cc251ff9417
Mika Huttunen <mika.huttunen@gofore.com>
2022-06-20 15:40:36 +0300

    OTR(Backend): OPHOTRKEH-63 split init script into 3 parts

---
+++ backend/otr/db/onr/2_onr_delete_learners.sql
@@ -0,0 +1,27 @@
+-- Sql script for deleting learners created in 0_onr_create_learners.sql if needed
+-- Running this script requires deletion of OTR contact details data first as done in 1_onr_init_contact_details.sql
+
+-- Code commented out so that it's not run by accident
+
+-- DELETE FROM yksilointivirhe WHERE henkilo_id IN (
+--    SELECT id
+--    FROM henkilo
+--    WHERE oidhenkilo IN (
+--        SELECT
+--            CASE i < 10
+--                WHEN TRUE THEN '1.2.246.562.24.3123450000' || i::text
+--                ELSE '1.2.246.562.24.312345000' || i::text
+--                END
+--        FROM generate_series(1, 53) AS i
+--    )
+--);
+
+--DELETE FROM henkilo
+--WHERE oidhenkilo IN (
+--    SELECT
+--        CASE i < 10
+--            WHEN TRUE THEN '1.2.246.562.24.3123450000' || i::text
+--            ELSE '1.2.246.562.24.312345000' || i::text
+--            END
+--    FROM generate_series(1, 53) AS i
+--);
