a85f8dafdd6f9e1c7a7a98bf181fb90c30be747d
Pejam Hassanzadeh <pejam.hassanzadeh@knowit.fi>
2022-08-05 14:01:02 +0300

    AKR:OTR:VKT:SHARED Upgrade Cypress to v10.4

--- frontend/packages/vkt/webpack.config.js
+++ frontend/packages/vkt/webpack.config.js
@@ -1,6 +1,6 @@
 const { merge } = require('webpack-merge');
 const common = require('../../webpack.common');
-// Draft
+
 module.exports = (env) => {
   const { getDefaults } = common('vkt', env, __dirname, 4002);
 
