093378d3281c0e81c37d3626e108cd6344df66bb
Pejam Hassanzadeh <pejam.hassanzadeh@knowit.fi>
2022-08-11 19:03:25 +0300

    AKR(Frontend): Delete actions and action types

--- frontend/packages/akr/src/redux/actions/authorisation.ts
+++
@@ -1,7 +0,0 @@
-import { Authorisation } from 'interfaces/authorisation';
-import { CLERK_TRANSLATOR_AUTHORISATION_ADD } from 'redux/actionTypes/authorisation';
-
-export const addAuthorisation = (authorisation: Authorisation) => ({
-  type: CLERK_TRANSLATOR_AUTHORISATION_ADD,
-  authorisation,
-});
--- frontend/packages/akr/src/redux/actions/publicUIView.ts
+++
@@ -1,7 +0,0 @@
-import { PublicUIViews } from 'enums/app';
-import { DISPLAY_PUBLIC_VIEW } from 'redux/actionTypes/publicUIView';
-
-export const setPublicUIView = (view: PublicUIViews) => ({
-  type: DISPLAY_PUBLIC_VIEW,
-  view,
-});
