82e46362e8c6dd35b5cfc6ede3cbad6e3be61f19
Tero Väntänen <tero.vantanen@vincit.fi>
2022-08-12 17:10:59 +0300

    OTR(Backend): OPHOTRKEH-111 Do not send expiry email for deleted qualifications

--- backend/otr/src/test/java/fi/oph/otr/scheduled/ExpiringQualificationsEmailCreatorTest.java
+++ backend/otr/src/test/java/fi/oph/otr/scheduled/ExpiringQualificationsEmailCreatorTest.java
@@ -54,18 +54,19 @@ public class ExpiringQualificationsEmailCreatorTest {
     final MeetingDate meetingDate = Factory.meetingDate();
     entityManager.persist(meetingDate);
 
-    final Qualification q1 = createQualification(meetingDate, date, false);
-    final Qualification q2 = createQualification(meetingDate, date.plusDays(10), false);
-    final Qualification q3 = createQualification(meetingDate, date.plusMonths(3), false);
+    final Qualification q1 = createQualification(meetingDate, date);
+    final Qualification q2 = createQualification(meetingDate, date.plusDays(10));
+    final Qualification q3 = createQualification(meetingDate, date.plusMonths(3));
 
-    final Qualification deletedQ = createQualification(meetingDate, date.plusDays(10), true);
-    final Qualification expiredQ = createQualification(meetingDate, date.minusDays(1), false);
-    final Qualification qExpiringLater = createQualification(meetingDate, date.plusMonths(3).plusDays(1), false);
+    final Qualification expiredQ = createQualification(meetingDate, date.minusDays(1));
+    final Qualification qExpiringLater = createQualification(meetingDate, date.plusMonths(3).plusDays(1));
+    final Qualification qDeleted = createQualification(meetingDate, date, true, false);
+    final Qualification iDeleted = createQualification(meetingDate, date, false, true);
 
-    final Qualification remindedQ1 = createQualification(meetingDate, date.plusMonths(1), false);
+    final Qualification remindedQ1 = createQualification(meetingDate, date.plusMonths(1));
     createQualificationReminder(remindedQ1);
 
-    final Qualification remindedQ2 = createQualification(meetingDate, date.plusMonths(2), false);
+    final Qualification remindedQ2 = createQualification(meetingDate, date.plusMonths(2));
     createQualificationReminder(remindedQ2);
     createQualificationReminder(remindedQ2);
 
@@ -82,18 +83,21 @@ public class ExpiringQualificationsEmailCreatorTest {
     assertTrue(expiringQIds.contains(q3.getId()));
   }
 
-  private Qualification createQualification(
-    final MeetingDate meetingDate,
-    final LocalDate endDate,
-    final boolean isDeleted
-  ) {
+  private Qualification createQualification(final MeetingDate meetingDate, final LocalDate endDate) {
+    return createQualification(meetingDate, endDate, false, false);
+  }
+
+  private Qualification createQualification(final MeetingDate meetingDate, final LocalDate endDate, final boolean qualificationDeleted, final boolean interpreterDeleted) {
     final Interpreter interpreter = Factory.interpreter();
+    if(interpreterDeleted) {
+      interpreter.markDeleted();
+    }
     entityManager.persist(interpreter);
 
     final Qualification qualification = Factory.qualification(interpreter, meetingDate);
     qualification.setBeginDate(endDate.minusYears(1));
     qualification.setEndDate(endDate);
-    if (isDeleted) {
+    if(qualificationDeleted) {
       qualification.markDeleted();
     }
     entityManager.persist(qualification);
