db4818450015bd93649bd91985a53c3bf49c93ca
Mika Huttunen <mika.huttunen@gofore.com>
2022-10-14 12:05:38 +0300

    AKR(Backend): OPHAKRKEH-453 updated email sender name

--- backend/akr/src/main/java/fi/oph/akr/config/Constants.java
+++ backend/akr/src/main/java/fi/oph/akr/config/Constants.java
@@ -3,7 +3,7 @@ package fi.oph.akr.config;
 public class Constants {
 
   public static final String CALLER_ID = "1.2.246.562.10.00000000001.akr";
-  public static final String EMAIL_SENDER_NAME = "AKR";
+  public static final String EMAIL_SENDER_NAME = "AKR | Opetushallitus";
   public static final String SERVICENAME = "akr";
   public static final String APP_ROLE = "APP_AKT";
 }
