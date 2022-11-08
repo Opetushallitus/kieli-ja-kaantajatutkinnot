f15eade20d4d689b0f2b56d7e8a3ee61762934bb
Mika Huttunen <mika.huttunen@gofore.com>
2022-11-08 13:22:26 +0200

    AKR & OTR: OPHOTRKEH-191 NotFoundPage implemented

---
+++ frontend/packages/otr/src/pages/NotFoundPage.tsx
@@ -0,0 +1,27 @@
+import { FC } from 'react';
+import { CustomButton, H1, Text } from 'shared/components';
+
+import { useAppTranslation, useCommonTranslation } from 'configs/i18n';
+import { AppRoutes } from 'enums/app';
+
+export const NotFoundPage: FC = () => {
+  const { t } = useAppTranslation({
+    keyPrefix: 'otr.pages.notFoundPage',
+  });
+  const translateCommon = useCommonTranslation();
+
+  return (
+    <div className="not-found-page">
+      <H1>{t('title')}</H1>
+      <Text>{t('description')}</Text>
+      <CustomButton
+        className="not-found-page__btn"
+        color="secondary"
+        variant="contained"
+        href={AppRoutes.PublicHomePage}
+      >
+        {translateCommon('frontPage')}
+      </CustomButton>
+    </div>
+  );
+};
