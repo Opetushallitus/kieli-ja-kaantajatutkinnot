1b753a2316e0d05e6a0cbc58760fc7e0bc4cbef3
Pyry Koivisto <pyry.koivisto@mavericks.fi>
2022-11-14 11:05:24 +0200

    OTR: Copy CSP improvements from VKT

--- backend/otr/src/test/java/fi/oph/otr/api/IndexControllerIntegrationTest.java
+++ backend/otr/src/test/java/fi/oph/otr/api/IndexControllerIntegrationTest.java
@@ -1,11 +1,17 @@
 package fi.oph.otr.api;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import fi.oph.otr.TestUtil;
 import java.io.IOException;
+import java.util.List;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 import javax.annotation.Resource;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -13,6 +19,11 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.StringTemplateResolver;
 
 @SpringBootTest
 @ActiveProfiles("test-hsql")
@@ -20,13 +31,19 @@ import org.springframework.test.web.servlet.MockMvc;
 class IndexControllerIntegrationTest {
 
   private static String expectedIndexHtml;
+  private static String expectedIndexHtmlTemplate;
+  private static SpringTemplateEngine templateEngine;
 
   @Resource
   private MockMvc mockMvc;
 
   @BeforeAll
   public static void loadExpectedIndexHtml() throws IOException {
-    expectedIndexHtml = TestUtil.readResourceAsString("static/index.html");
+    expectedIndexHtmlTemplate = TestUtil.readResourceAsString("static/index.html");
+    templateEngine = new SpringTemplateEngine();
+    final StringTemplateResolver templateResolver = new StringTemplateResolver();
+    templateResolver.setTemplateMode(TemplateMode.HTML);
+    templateEngine.setTemplateResolver(templateResolver);
   }
 
   @Test
@@ -50,8 +67,47 @@ class IndexControllerIntegrationTest {
     assertGetContent("/otr/static/assets/svg/footer_wave.svg", "image/svg+xml", expectedContent);
   }
 
-  private void assertIndexHtml(String url) throws Exception {
-    assertGetContent(url, "text/html;charset=UTF-8", expectedIndexHtml);
+  @Test
+  public void testDifferentNoncesAreReturnedForDifferentRequests() {
+    final List<String> urls = List.of("/", "/", "/foo", "/foo", "/");
+    final Set<String> nonces = urls
+      .stream()
+      .map(url -> {
+        try {
+          return readNonceFromCSPHeader(mockMvc.perform(get(url)).andReturn());
+        } catch (Exception e) {
+          throw new RuntimeException(e);
+        }
+      })
+      .collect(Collectors.toSet());
+    assertEquals(urls.size(), nonces.size());
+  }
+
+  private void assertIndexHtml(final String url) throws Exception {
+    mockMvc
+      .perform(get(url))
+      .andDo(res -> fillNonceIntoTemplate(readNonceFromCSPHeader(res)))
+      .andExpect(status().isOk())
+      .andExpect(content().contentType("text/html;charset=UTF-8"))
+      .andExpect((content().string(expectedIndexHtml)));
+  }
+
+  private static String readNonceFromCSPHeader(final MvcResult res) {
+    final String cspDirective = res.getResponse().getHeader("Content-Security-Policy");
+    if (cspDirective != null) {
+      final Pattern p = Pattern.compile("'nonce-([A-Za-z0-9+/]+=*)'");
+      final Matcher m = p.matcher(cspDirective);
+      m.find();
+      return m.group(1);
+    } else {
+      return null;
+    }
+  }
+
+  private void fillNonceIntoTemplate(final String nonce) {
+    final Context ctx = new Context();
+    ctx.setVariable("cspNonce", nonce);
+    expectedIndexHtml = templateEngine.process(expectedIndexHtmlTemplate, ctx);
   }
 
   private void assertGetContent(String url, String expectedContentType, String expectedContent) throws Exception {
