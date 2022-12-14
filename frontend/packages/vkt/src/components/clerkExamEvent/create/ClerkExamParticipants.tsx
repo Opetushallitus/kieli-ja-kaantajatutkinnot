1c99c7162282d3054bd558711a8b1c7d3557389c
Jarkko Pesonen <435495+jrkkp@users.noreply.github.com>
2022-12-14 14:30:54 +0200

    VKT(Frontend): OPHVKTKEH-72 change participants combobox to textfield

--- frontend/packages/vkt/src/components/clerkExamEvent/create/ClerkExamParticipants.tsx
+++ frontend/packages/vkt/src/components/clerkExamEvent/create/ClerkExamParticipants.tsx
@@ -1,14 +1,11 @@
-import { ComboBox, H3 } from 'shared/components';
-import { TextFieldVariant } from 'shared/enums';
-import { AutocompleteValue } from 'shared/interfaces';
+import { ChangeEvent } from 'react';
+import { CustomTextField, H3 } from 'shared/components';
+import { TextFieldTypes, TextFieldVariant } from 'shared/enums';
 
 import { useClerkTranslation, useCommonTranslation } from 'configs/i18n';
 import { useAppDispatch } from 'configs/redux';
 import { DraftClerkExamEvent } from 'interfaces/clerkExamEvent';
 import { updateClerkNewExamDate } from 'redux/reducers/clerkNewExamDate';
-import { ExamCreateEventUtils } from 'utils/examCreateEvent';
-
-const maxParticipantsOpts = ExamCreateEventUtils.maxParticipantsOpts();
 
 export const ClerkExamParticipants = ({
   examForm,
@@ -21,25 +18,31 @@ export const ClerkExamParticipants = ({
   const translateCommon = useCommonTranslation();
   const dispatch = useAppDispatch();
 
-  const onParticipantsChange = ({}, num: AutocompleteValue) => {
-    const examFormDetails: DraftClerkExamEvent = {
-      ...examForm,
-      maxParticipants: Number(num?.value),
-    };
-    dispatch(updateClerkNewExamDate(examFormDetails));
+  const onParticipantsChange = (
+    event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const value = Number(event.target.value);
+
+    if (value > 0 && value < 999) {
+      const examFormDetails: DraftClerkExamEvent = {
+        ...examForm,
+        maxParticipants: value,
+      };
+      dispatch(updateClerkNewExamDate(examFormDetails));
+    }
   };
 
   return (
     <div className="rows gapped">
       <H3>{t('fillingsTotal')}</H3>
-      <ComboBox
-        data-testid="clerk-exam__event-information__max-participants"
-        autoHighlight
+      <CustomTextField
+        className="clerk-exam-create-participants"
         label={translateCommon('choose')}
-        onChange={onParticipantsChange}
+        type={TextFieldTypes.Number}
+        value={examForm.maxParticipants}
         variant={TextFieldVariant.Outlined}
-        values={maxParticipantsOpts}
-        value={ExamCreateEventUtils.getParticipantsComboOpt(examForm)}
+        onChange={onParticipantsChange}
+        data-testid="clerk-exam__event-information__max-participants"
       />
     </div>
   );
