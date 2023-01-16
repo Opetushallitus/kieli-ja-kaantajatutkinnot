eabce9ddbb7c63b3ad7c5e22c8071bd214f5c534
Mika Huttunen <mika.huttunen@gofore.com>
2023-01-16 15:36:39 +0200

    OTR(Backend): OPHOTRKEH-197 BaseRepository added

--- backend/otr/src/main/java/fi/oph/otr/repository/InterpreterRepository.java
+++ backend/otr/src/main/java/fi/oph/otr/repository/InterpreterRepository.java
@@ -2,12 +2,11 @@ package fi.oph.otr.repository;
 
 import fi.oph.otr.model.Interpreter;
 import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface InterpreterRepository extends JpaRepository<Interpreter, Long> {
+public interface InterpreterRepository extends BaseRepository<Interpreter> {
   @Query("SELECT i.onrId FROM Interpreter i")
   List<String> listAllOnrIds();
 }
--- backend/otr/src/main/java/fi/oph/otr/repository/QualificationRepository.java
+++ backend/otr/src/main/java/fi/oph/otr/repository/QualificationRepository.java
@@ -5,12 +5,11 @@ import fi.oph.otr.model.Qualification;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface QualificationRepository extends JpaRepository<Qualification, Long> {
+public interface QualificationRepository extends BaseRepository<Qualification> {
   @Query(
     "SELECT q" +
     " FROM Qualification q" +
