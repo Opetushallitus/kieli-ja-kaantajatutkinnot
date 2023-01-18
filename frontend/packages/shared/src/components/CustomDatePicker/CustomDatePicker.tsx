9c248491b07efe223e38dd38a3b96f42d7a0fd9c
Jarkko Pesonen <435495+jrkkp@users.noreply.github.com>
2023-01-18 13:25:28 +0200

    SHARED(Frontend): OPHVKTKEH-75 Add error labels to datepicker component

--- frontend/packages/shared/src/components/CustomDatePicker/CustomDatePicker.tsx
+++ frontend/packages/shared/src/components/CustomDatePicker/CustomDatePicker.tsx
@@ -1,9 +1,9 @@
-import TextField from '@mui/material/TextField';
+import { CustomTextFieldProps, CustomTextField } from 'components';
 import { DesktopDatePicker, LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import dayjs, { Dayjs } from 'dayjs';
 
-interface DatePickerProps {
+interface DatePickerProps extends CustomTextFieldProps {
   value: Dayjs | null;
   setValue: (value: Dayjs | null) => void;
   label?: string;
@@ -21,6 +21,9 @@ export const CustomDatePicker = ({
   minDate,
   maxDate,
   showToolbar = true,
+  error,
+  helperText,
+  showHelperText,
 }: DatePickerProps): JSX.Element => {
   const MIN_DATE = '1980-01-01';
   const MAX_DATE = '2050-01-01';
@@ -43,7 +46,16 @@ export const CustomDatePicker = ({
         disabled={disabled}
         minDate={minDate ?? dayjs(MIN_DATE)}
         maxDate={maxDate ?? dayjs(MAX_DATE)}
-        renderInput={(params) => <TextField {...params} />}
+        renderInput={(params) => {
+          const textFieldParams = {
+            ...params,
+            error,
+            helperText,
+            showHelperText,
+          };
+
+          return <CustomTextField {...textFieldParams} />;
+        }}
       />
     </LocalizationProvider>
   );
