6cfb0f8531dc0329c50a461008b7d1a598b16d59
Mika Huttunen <mika.huttunen@gofore.com>
2023-01-23 17:30:23 +0200

    VKT(Frontend): OPHVKTKEH-112 stepper and reservation details in Done step of enrollment

--- frontend/packages/vkt/src/redux/sagas/publicEnrollment.ts
+++ frontend/packages/vkt/src/redux/sagas/publicEnrollment.ts
@@ -92,7 +92,6 @@ function* loadPublicEnrollmentSaveSaga(
 
     yield call(axiosInstance.post, saveUrl, body);
 
-    yield put(setPublicUIView(PublicUIViews.EnrollmentComplete));
     yield put(storePublicEnrollmentSave());
   } catch (error) {
     const errorMessage = NotifierUtils.getAPIErrorMessage(error as AxiosError);
