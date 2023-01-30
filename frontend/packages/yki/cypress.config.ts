b0794246c3d78a5c74f3051337d7008a4f597ddd
Pyry Koivisto <pyry.koivisto@mavericks.fi>
2023-01-30 17:21:14 +0200

    YKI(Frontend): Copy and customize project specific configurations

---
+++ frontend/packages/yki/cypress.config.ts
@@ -0,0 +1,29 @@
+import webpack from '@cypress/webpack-preprocessor';
+import { defineConfig } from 'cypress';
+
+// eslint-disable-next-line no-restricted-imports
+import webpackConfigs from './webpack.config';
+
+export default defineConfig({
+  e2e: {
+    baseUrl: 'http://localhost:4003',
+    video: false,
+    defaultCommandTimeout: 15000,
+    chromeWebSecurity: false,
+    retries: 1,
+    fixturesFolder: './src/tests/cypress/fixtures/json',
+    screenshotsFolder: './src/tests/cypress/screenshots',
+    videosFolder: './src/tests/cypress/videos',
+    downloadsFolder: './src/tests/cypress/downloads',
+    supportFile: './src/tests/cypress/support/index.ts',
+    specPattern: './src/tests/cypress/integration/**/*.spec.{js,jsx,ts,tsx}',
+    setupNodeEvents(on) {
+      const options = {
+        webpackOptions: webpackConfigs({ cypress: true }),
+      };
+
+      // @ts-expect-error: options are compatible with webpackOptions
+      on('file:preprocessor', webpack(options));
+    },
+  },
+});
