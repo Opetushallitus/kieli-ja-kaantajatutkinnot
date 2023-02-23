e13b61c2ff1bbd869a75cb6c268583d8bc210bd7
Pyry Koivisto <pyry.koivisto@mavericks.fi>
2023-02-23 16:04:03 +0200

    YKI(Frontend): review fixes

--- frontend/packages/yki/src/components/registration/examSession/PublicExamSessionListingRow.tsx
+++ frontend/packages/yki/src/components/registration/examSession/PublicExamSessionListingRow.tsx
@@ -13,16 +13,12 @@ export const PublicExamSessionListingRow = ({
   const { t } = usePublicTranslation({
     keyPrefix: 'yki.pages.registrationPage',
   });
-  const handleRowClick = () =>
-    // eslint-disable-next-line no-console
-    console.log('clicked on examSession', examSession.id);
   const locationInfo = ExamUtils.getLocationInfo(examSession, getCurrentLang());
 
   return (
     <TableRow
       className="cursor-pointer"
       data-testid={`public-exam-session__id-${examSession.id}-row`}
-      onClick={handleRowClick}
     >
       <TableCell>{ExamUtils.renderLanguageAndLevel(examSession)}</TableCell>
       <TableCell>
@@ -42,8 +38,7 @@ export const PublicExamSessionListingRow = ({
       </TableCell>
       <TableCell>{examSession.exam_fee} €</TableCell>
       <TableCell>
-        {examSession.max_participants - (examSession.participants ?? 0)} /{' '}
-        {examSession.max_participants}
+        {examSession.max_participants - (examSession.participants ?? 0)}
       </TableCell>
       <TableCell>{t('register')}</TableCell>
     </TableRow>
