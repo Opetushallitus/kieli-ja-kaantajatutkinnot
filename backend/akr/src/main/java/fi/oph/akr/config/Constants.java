565e8f623ab3be3f330532feddcf3ff0abe335a0
Mika Huttunen <mika.huttunen@hotmail.fi>
2023-03-06 11:37:36 +0200

    AKR:OTR(Backend): crons for daily scheduled tasks defined under Constants

--- backend/akr/src/main/java/fi/oph/akr/config/Constants.java
+++ backend/akr/src/main/java/fi/oph/akr/config/Constants.java
@@ -6,4 +6,13 @@ public class Constants {
   public static final String EMAIL_SENDER_NAME = "AKR | Opetushallitus";
   public static final String SERVICENAME = "akr";
   public static final String APP_ROLE = "APP_AKT";
+
+  // Daily at 9:00
+  public static final String EVICT_PUBLIC_TRANSLATORS_CACHE_CRON = "0 0 9 * * *";
+  // Daily at 9:30
+  public static final String GENERATE_STATISTICS_CRON = "0 30 9 * * *";
+  // Daily at 10:00
+  public static final String CHECK_EXPIRING_AUTHORISATIONS_CRON = "0 0 10 * * *";
+  // Daily at 10:30
+  public static final String DESTROY_OBSOLETE_CONTACT_REQUESTS_CRON = "0 30 10 * * *";
 }
