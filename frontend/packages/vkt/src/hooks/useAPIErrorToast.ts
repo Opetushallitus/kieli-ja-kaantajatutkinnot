7c2684499543fedd49637d6a7628d3c041fe7584
Jarkko Pesonen <435495+jrkkp@users.noreply.github.com>
2023-05-24 16:04:04 +0300

    VKT(Backend & Frontend) OPHVKTKEH-151 error handling for redirects

--- frontend/packages/vkt/src/hooks/useAPIErrorToast.ts
+++ frontend/packages/vkt/src/hooks/useAPIErrorToast.ts
@@ -5,10 +5,16 @@ import { useToast } from 'shared/hooks';
 import { useAppDispatch, useAppSelector } from 'configs/redux';
 import { resetAPIError } from 'redux/reducers/APIError';
 import { APIErrorSelector } from 'redux/selectors/APIError';
+import { NotifierUtils } from 'utils/notifier';
 
 export const useAPIErrorToast = () => {
   const dispatch = useAppDispatch();
-  const errorMessage = useAppSelector(APIErrorSelector).message;
+  const params = new URLSearchParams(window.location.search);
+  const errorMessageQuery = params.get('error')
+    ? NotifierUtils.getURLErrorMessage(params.get('error'))
+    : null;
+  const errorMessage =
+    useAppSelector(APIErrorSelector).message || errorMessageQuery;
   const { showToast } = useToast();
 
   useEffect(() => {
