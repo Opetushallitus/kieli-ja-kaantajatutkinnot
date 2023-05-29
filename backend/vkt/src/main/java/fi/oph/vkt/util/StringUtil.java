d96a5657339a22fd9456511051055a2798c004a4
Jarkko Pesonen <435495+jrkkp@users.noreply.github.com>
2023-05-29 10:51:00 +0300

    VKT(Backend) OPHVKTKEH-175: Sanitize enrollment input

--- backend/vkt/src/main/java/fi/oph/vkt/util/StringUtil.java
+++ backend/vkt/src/main/java/fi/oph/vkt/util/StringUtil.java
@@ -1,8 +1,18 @@
 package fi.oph.vkt.util;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.safety.Safelist;
+
 public class StringUtil {
 
   public static String trim(final String nullable) {
     return nullable != null ? nullable.trim() : null;
+  }
+
+  public static String sanitize(final String nullable) {
+    return nullable != null
+      ? trim(Jsoup.clean(nullable, "", Safelist.none(), new Document.OutputSettings().prettyPrint(false)))
+      : null;
   }
 }
--- backend/vkt/src/test/java/fi/oph/vkt/service/PublicEnrollmentServiceTest.java
+++ backend/vkt/src/test/java/fi/oph/vkt/service/PublicEnrollmentServiceTest.java
@@ -15,6 +15,7 @@ import static org.mockito.Mockito.when;
 
 import fi.oph.vkt.Factory;
 import fi.oph.vkt.api.dto.PublicEnrollmentCreateDTO;
+import fi.oph.vkt.api.dto.PublicEnrollmentDTO;
 import fi.oph.vkt.api.dto.PublicEnrollmentInitialisationDTO;
 import fi.oph.vkt.api.dto.PublicExamEventDTO;
 import fi.oph.vkt.api.dto.PublicPersonDTO;
@@ -352,6 +353,41 @@ public class PublicEnrollmentServiceTest {
 
     verify(publicEnrollmentEmailServiceMock, times(1)).sendEnrollmentConfirmationEmail(any(), any());
     verify(publicEnrollmentEmailServiceMock, times(0)).sendEnrollmentToQueueConfirmationEmail(any(), any());
+  }
+
+  @Test
+  public void testCreateEnrollmentSanitizeHTML() {
+    final ExamEvent examEvent = Factory.examEvent();
+    final Person person = Factory.person();
+    final Reservation reservation = Factory.reservation(examEvent, person);
+
+    entityManager.persist(examEvent);
+    entityManager.persist(person);
+    entityManager.persist(reservation);
+
+    final PublicEnrollmentCreateDTO dto = createDTOBuilder()
+      .previousEnrollment("<b>Foobar</b>")
+      .email("<a>foo@bar.foo")
+      .phoneNumber("<asd>04012345")
+      .street("<script>alert('baa')</script>")
+      .postalCode("<body>00000</body>")
+      .town("<i>Kaupunki</i>")
+      .country("<3")
+      .digitalCertificateConsent(false)
+      .build();
+
+    final PublicEnrollmentDTO publicEnrollmentDTO = publicEnrollmentService.createEnrollment(
+      dto,
+      reservation.getId(),
+      person
+    );
+    assertEquals("Foobar", publicEnrollmentDTO.previousEnrollment());
+    assertEquals("foo@bar.foo", publicEnrollmentDTO.email());
+    assertEquals("04012345", publicEnrollmentDTO.phoneNumber());
+    assertEquals("", publicEnrollmentDTO.street());
+    assertEquals("00000", publicEnrollmentDTO.postalCode());
+    assertEquals("Kaupunki", publicEnrollmentDTO.town());
+    assertEquals("&lt;3", publicEnrollmentDTO.country());
   }
 
   private PublicEnrollmentCreateDTO.PublicEnrollmentCreateDTOBuilder createDTOBuilder() {
