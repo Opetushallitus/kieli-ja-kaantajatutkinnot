e2ecd0146735eaf7b7f134138d181d2c7168224a
Jarkko Pesonen <435495+jrkkp@users.noreply.github.com>
2023-07-06 14:50:32 +0300

    VKT(Backend): Prettier fix [deploy]

--- backend/vkt/src/main/java/fi/oph/vkt/api/PublicController.java
+++ backend/vkt/src/main/java/fi/oph/vkt/api/PublicController.java
@@ -28,7 +28,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.function.Function;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
