e02991723367473d8decda15ced08ece82f3aede
Jarkko Pesonen <435495+jrkkp@users.noreply.github.com>
2023-07-07 11:23:29 +0300

    VKT(Backend): Lock timout does not work with psql. Use native query

--- backend/vkt/src/main/java/fi/oph/vkt/repository/PaymentRepository.java
+++ backend/vkt/src/main/java/fi/oph/vkt/repository/PaymentRepository.java
@@ -5,13 +5,13 @@ import jakarta.persistence.LockModeType;
 import jakarta.persistence.QueryHint;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.QueryHints;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PaymentRepository extends BaseRepository<Payment> {
-  @Lock(LockModeType.PESSIMISTIC_WRITE)
-  @QueryHints({ @QueryHint(name = "jakarta.persistence.lock.timeout", value = "3000") })
-  @Override
-  Optional<Payment> findById(final Long paymentId);
+  @Query(value = "SELECT * FROM payment p WHERE p.payment_id = :paymentId FOR UPDATE", nativeQuery = true)
+  Optional<Payment> findWithLockingById(@Param("paymentId") final Long paymentId);
 }
