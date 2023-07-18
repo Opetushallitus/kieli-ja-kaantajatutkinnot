cdd5b82fbde380d409829c149ede617e0534b860
Pyry Koivisto <pyry.koivisto@mavericks.fi>
2023-07-18 13:38:09 +0300

    AKR(Frontend): Fix tests [deploy]

--- frontend/packages/akr/src/tests/cypress/integration/lang_selector.spec.ts
+++ frontend/packages/akr/src/tests/cypress/integration/lang_selector.spec.ts
@@ -1,19 +1,22 @@
 import { onLangSelector } from 'tests/cypress/support/page-objects/langSelector';
 
 describe('LangSelector', () => {
-  it('should show the Finnish language as a default language', () => {
-    cy.openPublicHomePage();
-
-    onLangSelector.elements.langSelector().should('contain.text', 'Suomeksi');
-  });
-
   it('should change the language', () => {
-    // arrange
     cy.openPublicHomePage();
-    // act
-    onLangSelector.clickLangSelector();
-    onLangSelector.selectLangOption('in english');
-    // assert
-    onLangSelector.elements.langSelector().should('contain.text', 'In English');
+    onLangSelector
+      .languageOption('Suomeksi')
+      .should('have.attr', 'aria-current', 'true');
+    onLangSelector
+      .languageOption('In English')
+      .should('have.attr', 'aria-current', 'false');
+
+    onLangSelector.languageOption('In English').click();
+
+    onLangSelector
+      .languageOption('Suomeksi')
+      .should('have.attr', 'aria-current', 'false');
+    onLangSelector
+      .languageOption('In English')
+      .should('have.attr', 'aria-current', 'true');
   });
 });
