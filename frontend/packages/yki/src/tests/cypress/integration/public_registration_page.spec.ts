c59070d8562ddc64e35984587048cbe1466886b2
Pyry Koivisto <pyry.koivisto@mavericks.fi>
2023-08-02 17:24:24 +0300

    YKI(Frontend): Regressions tests for public registration filters

--- frontend/packages/yki/src/tests/cypress/integration/public_registration_page.spec.ts
+++ frontend/packages/yki/src/tests/cypress/integration/public_registration_page.spec.ts
@@ -1,4 +1,5 @@
 import { onPublicRegistrationPage } from 'tests/cypress/support/page-objects/publicRegistrationPage';
+import { findDialogByText } from 'tests/cypress/support/utils/dialog';
 
 describe('PublicRegistrationPage', () => {
   beforeEach(() => {
@@ -7,5 +8,36 @@ describe('PublicRegistrationPage', () => {
 
   it('is visible', () => {
     onPublicRegistrationPage.isVisible();
+  });
+
+  describe('allows filtering exams', () => {
+    it('but filter criteria must be selected first', () => {
+      onPublicRegistrationPage.showResults();
+      const dialogHeading = 'Valitse tutkinnon kieli ja taitotaso';
+      findDialogByText(dialogHeading).should('be.visible');
+      findDialogByText(dialogHeading)
+        .findByText('takaisin', { exact: false })
+        .click();
+      cy.findByRole('dialog').should('not.exist');
+    });
+
+    it('all results are available initially', () => {
+      onPublicRegistrationPage.expectResultsCount(10);
+      onPublicRegistrationPage.selectExamLanguage('kaikki kielet');
+      onPublicRegistrationPage.selectExamLevel('kaikki tasot');
+      onPublicRegistrationPage.expectResultsCount(10);
+      onPublicRegistrationPage.showResults();
+      // TODO Verify returned rows?
+    });
+
+    it('can filter by current availability', () => {
+      onPublicRegistrationPage.clearAll();
+      onPublicRegistrationPage.selectExamLanguage('kaikki kielet');
+      onPublicRegistrationPage.selectExamLevel('kaikki tasot');
+      onPublicRegistrationPage.toggleShowOnlyIfAvailablePlaces();
+      onPublicRegistrationPage.expectResultsCount(7);
+      onPublicRegistrationPage.toggleShowOnlyIfOngoingAdmission();
+      onPublicRegistrationPage.expectResultsCount(2);
+    });
   });
 });
--- frontend/packages/yki/src/tests/cypress/support/page-objects/publicRegistrationPage.ts
+++ frontend/packages/yki/src/tests/cypress/support/page-objects/publicRegistrationPage.ts
@@ -1,10 +1,55 @@
+import { selectComboBoxOptionByName } from 'tests/cypress/support/utils/comboBox';
+
 class PublicRegistrationPage {
   elements = {
+    clearAllButton: () => cy.contains('Tyhjennä kaikki'),
+    filterByLanguage: () =>
+      cy.findByRole('combobox', { name: /Valitse kieli/ }),
+    filterByLevel: () =>
+      cy.findByRole('combobox', { name: /Valitse taitotaso/ }),
+    resultList: () => cy.findByRole(''),
+    showOnlyIfAvailablePlaces: () => cy.findByLabelText('Näytä vain kielitutkinnot, joissa on tilaa'),
+    showOnlyIfOngoingAdmission: () => cy.findByLabelText('Näytä vain kielitutkinnot, joihin voi ilmoittautua nyt'),
+    showResultsButton: () => cy.contains('Näytä tulokset'),
     title: () => cy.findByTestId('public-registration-page__title-heading'),
   };
 
+  clearAll() {
+    this.elements.clearAllButton().click();
+  }
+
+  displaysErrorDialog(errorText: string) {
+    cy.findByRole('dialog').contains(errorText).should('be.visible');
+  }
+
+  expectResultsCount(count: number) {
+    this.elements
+      .showResultsButton()
+      .should('have.text', `Näytä tulokset (${count})`);
+  }
+
   isVisible() {
     this.elements.title().should('be.visible');
+  }
+
+  selectExamLanguage(language: string) {
+    selectComboBoxOptionByName(this.elements.filterByLanguage(), language);
+  }
+
+  selectExamLevel(level: string) {
+    selectComboBoxOptionByName(this.elements.filterByLevel(), level);
+  }
+
+  showResults() {
+    this.elements.showResultsButton().click();
+  }
+
+  toggleShowOnlyIfAvailablePlaces() {
+    this.elements.showOnlyIfAvailablePlaces().click();
+  }
+
+  toggleShowOnlyIfOngoingAdmission() {
+    this.elements.showOnlyIfOngoingAdmission().click();
   }
 }
 
