98ed6d449b15768012d0e3884aa64cc77eba27f5
Mika Huttunen <mika.huttunen@hotmail.fi>
2023-08-04 13:32:01 +0300

    AKR:VKT:YKI:Shared jest snapshots updated

--- frontend/packages/shared/src/components/ComboBox/ComboBox.test.tsx
+++ frontend/packages/shared/src/components/ComboBox/ComboBox.test.tsx
@@ -1,6 +1,6 @@
 import renderer from 'react-test-renderer';
 
-import { TextFieldVariant } from '../../enums/common';
+import { TextFieldVariant } from '../../enums';
 import { ComboBox } from './ComboBox';
 
 describe('ComboBox', () => {
