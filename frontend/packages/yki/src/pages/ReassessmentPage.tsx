11f4bcbc42a5245f53306b253968b45e57f73a6c
Laura Ketola <laura.ketola@gofore.com>
2023-09-22 22:46:51 +0300

    YKI(Frontend): Change reassessment page texts

--- frontend/packages/yki/src/pages/ReassessmentPage.tsx
+++ frontend/packages/yki/src/pages/ReassessmentPage.tsx
@@ -1,131 +1,21 @@
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import {
-  Accordion,
-  AccordionDetails,
-  AccordionSummary,
-  Grid,
-  Paper,
-} from '@mui/material';
+import { Grid, Paper } from '@mui/material';
 import { Box } from '@mui/system';
 import { FC, useEffect } from 'react';
 import { H1, H2, HeaderSeparator, Text } from 'shared/components';
 import { APIResponseStatus } from 'shared/enums';
-import { useWindowProperties } from 'shared/hooks';
 
 import { PublicEvaluationPeriodListing } from 'components/reassessment/PublicEvaluationPeriodListing';
 import { PublicEvaluationPeriodListingSkeleton } from 'components/skeletons/PublicEvaluationPeriodListingSkeleton';
-import { useCommonTranslation, usePublicTranslation } from 'configs/i18n';
+import { usePublicTranslation } from 'configs/i18n';
 import { useAppDispatch, useAppSelector } from 'configs/redux';
 import { loadEvaluationPeriods } from 'redux/reducers/evaluationPeriods';
 import { evaluationPeriodsSelector } from 'redux/selectors/evaluationPeriods';
-
-const PricingBulletList = () => {
-  const translateCommon = useCommonTranslation();
-  const examParts = [
-    'readingComprehension',
-    'speechComprehension',
-    'writing',
-    'speaking',
-  ];
-
-  return (
-    <ul>
-      <Text>
-        {examParts.map((key, i) => (
-          <li key={i}>{translateCommon(`examParts.${key}`)} 50 €</li>
-        ))}
-      </Text>
-    </ul>
-  );
-};
-
-const PricingAccordion = () => {
-  const { t } = usePublicTranslation({
-    keyPrefix: 'yki.pages.reassessmentPage.info.pricing',
-  });
-
-  return (
-    <Accordion>
-      <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-        <Text className="bold">{t('heading')}</Text>
-      </AccordionSummary>
-      <AccordionDetails>
-        <Text>{t('body1')}</Text>
-        <Text className="bold">{t('body2')}</Text>
-        <PricingBulletList />
-        <Text>{t('body3')}</Text>
-        <Text>{t('body4')}</Text>
-      </AccordionDetails>
-    </Accordion>
-  );
-};
-
-const PricingSection = () => {
-  const { t } = usePublicTranslation({
-    keyPrefix: 'yki.pages.reassessmentPage.info.pricing',
-  });
-
-  return (
-    <>
-      <H2 className="public-reassessment-page__info__section__heading-title">
-        {t('heading')}
-      </H2>
-      <Text>{t('body1')}</Text>
-      <Text className="bold">{t('body2')}</Text>
-      <PricingBulletList />
-      <Text>{t('body3')}</Text>
-      <Text>{t('body4')}</Text>
-    </>
-  );
-};
-
-const ScheduleAccordion = () => {
-  const { t } = usePublicTranslation({
-    keyPrefix: 'yki.pages.reassessmentPage.info.schedule',
-  });
-
-  return (
-    <Accordion>
-      <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-        <Text className="bold"> {t('heading')}</Text>
-      </AccordionSummary>
-      <AccordionDetails>
-        <Text>{t('body1')}</Text>
-        <Text>{t('body2')}</Text>
-        <br />
-        <Text>{t('body3')}</Text>
-        <Text>{t('body4')}</Text>
-      </AccordionDetails>
-    </Accordion>
-  );
-};
-
-const ScheduleSection = () => {
-  const { t } = usePublicTranslation({
-    keyPrefix: 'yki.pages.reassessmentPage.info.schedule',
-  });
-
-  return (
-    <>
-      <H2 className="public-reassessment-page__info__section__heading-title">
-        {t('heading')}
-      </H2>
-      <Text>{t('body1')}</Text>
-      <Text>{t('body2')}</Text>
-      <br />
-      <Text>
-        {t('body3')} {t('body4')}
-      </Text>
-    </>
-  );
-};
 
 export const ReassessmentPage: FC = () => {
   const { t } = usePublicTranslation({
     keyPrefix: 'yki.pages.reassessmentPage',
   });
 
-  const { isPhone } = useWindowProperties();
   const dispatch = useAppDispatch();
   const { status } = useAppSelector(evaluationPeriodsSelector);
 
@@ -153,7 +43,7 @@ export const ReassessmentPage: FC = () => {
           <HeaderSeparator />
           <Text>{t('introduction.info')}</Text>
           <br />
-          <Text className="bold">{t('introduction.fee')}</Text>
+          <Text>{t('introduction.timeLimit')}</Text>
         </Grid>
         <Grid
           item
@@ -165,26 +55,26 @@ export const ReassessmentPage: FC = () => {
                 {t('info.general.heading')}
               </H2>
               <Text>{t('info.general.body1')}</Text>
-              <Text className="bold margin-top-lg">
+              <br />
+              <Text>
                 {t('info.general.body2')}
+                <a href="{t('info.general.link')}">{t('info.general.link')}</a>
               </Text>
+              <br />
               <Text>{t('info.general.body3')}</Text>
             </div>
-            {isPhone ? (
-              <div className="public-reassessment-page__info__accordions">
-                <PricingAccordion />
-                <ScheduleAccordion />
-              </div>
-            ) : (
-              <>
-                <div className="public-reassessment-page__info__section">
-                  <PricingSection />
-                </div>
-                <div className="public-reassessment-page__info__section">
-                  <ScheduleSection />
-                </div>
-              </>
-            )}
+            <div className="public-reassessment-page__priicng__section">
+              <H2 className="public-reassessment-page__info__section__heading-title">
+                {t('info.pricing.heading')}
+              </H2>
+              <Text>{t('info.pricing.body1')}</Text>
+              <br />
+              <Text>{t('info.pricing.body2')}</Text>
+              <br />
+              <Text>{t('info.pricing.body3')}</Text>
+              <br />
+              <Text>{t('info.pricing.body4')}</Text>
+            </div>
           </Paper>
         </Grid>
         <Grid
