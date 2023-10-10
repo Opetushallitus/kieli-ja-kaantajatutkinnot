aea186a6aae8ae16885484816c4320a61f1f5a74
Pyry Koivisto <pyry.koivisto@mavericks.fi>
2023-10-10 10:35:24 +0300

    YKI(Frontend): Return evaluation period details from msw

--- frontend/packages/yki/src/tests/msw/handlers.ts
+++ frontend/packages/yki/src/tests/msw/handlers.ts
@@ -9,6 +9,17 @@ export const handlers = [
   rest.get(APIEndpoints.Evaluations, (_, res, ctx) => {
     return res(ctx.status(200), ctx.json(evaluationPeriods));
   }),
+  rest.get(APIEndpoints.Evaluation, (req, res, ctx) => {
+    const { evaluationId } = req.params;
+    const evaluationPeriod = evaluationPeriods.evaluation_periods.filter(
+      (ep) => ep.id === Number(evaluationId)
+    )[0];
+    if (evaluationPeriod) {
+      return res(ctx.status(200), ctx.json(evaluationPeriod));
+    } else {
+      return res(ctx.status(404));
+    }
+  }),
   rest.get(APIEndpoints.ExamSessions, (_, res, ctx) => {
     return res(ctx.status(200), ctx.json(examSessions));
   }),
