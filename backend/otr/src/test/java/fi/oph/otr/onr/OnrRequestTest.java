f2437f5ab9273725801e5a22572b638be9057c9f
Jarkko Pesonen <435495+jrkkp@users.noreply.github.com>
2023-10-23 15:44:21 +0300

    OTR(Backend) OPHOTRKEH-248: test for ONR response deserialization

--- backend/otr/src/test/java/fi/oph/otr/onr/OnrRequestTest.java
+++ backend/otr/src/test/java/fi/oph/otr/onr/OnrRequestTest.java
@@ -1,29 +1,32 @@
 package fi.oph.otr.onr;
 
-import fi.oph.otr.onr.model.PersonalData;
-import fi.vm.sade.javautils.nio.cas.CasClient;
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import java.io.IOException;
-import java.util.Optional;
-import org.asynchttpclient.Response;
-import org.springframework.beans.factory.annotation.Value;
-
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import fi.oph.otr.onr.model.PersonalData;
+import fi.vm.sade.javautils.nio.cas.CasClient;
+import java.io.IOException;
+import java.util.Optional;
+import org.asynchttpclient.Response;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.security.test.context.support.WithMockUser;
+
+@WithMockUser
+@DataJpaTest
 class OnrRequestTest {
 
-  @Value("classpath:payment/henkilo-hetu-response.json")
+  @Value("classpath:json/henkilo-hetu-response.json")
   private org.springframework.core.io.Resource hetuRequestResponse;
 
   @Test
   void testFindPersonByIdentityNumberDeserializes() throws Exception {
     final Response response = mock(Response.class);
     final CasClient casClient = mock(CasClient.class);
-    final OnrOperationApiImpl onrOperationApi = new OnrOperationApiImpl(casClient, "");
+    final OnrOperationApiImpl onrOperationApi = new OnrOperationApiImpl(casClient, "http://localhost");
 
     when(casClient.executeBlocking(any())).thenReturn(response);
     when(response.getStatusCode()).thenReturn(200);
