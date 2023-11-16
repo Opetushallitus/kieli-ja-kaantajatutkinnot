9d0c83382564d12edb1d24ffcb94d98cf104c52c
Mark <mark.sevalnev@knowit.fi>
2023-11-16 14:14:37 +0200

    AKR (Backend & Frontend): OPHAKRKEH-522 updated Jest snapshot test [deploy]

--- backend/akr/src/main/java/fi/oph/akr/model/Translator.java
+++ backend/akr/src/main/java/fi/oph/akr/model/Translator.java
@@ -65,7 +65,6 @@ public class Translator extends BaseEntity {
 
   @Column(name = "country")
   private String country;
-
   //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
   @Column(name = "extra_information")
