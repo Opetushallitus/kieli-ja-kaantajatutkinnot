0a9c816b7ed84b1c3e3d811503901cb10623e5e5
Pyry Koivisto <pyry.koivisto@mavericks.fi>
2024-03-07 10:32:13 +0200

    YKI(Frontend): Implement view to continue to registration as authenticated user

--- frontend/packages/yki/src/components/registration/PublicIdentificationGrid.tsx
+++ frontend/packages/yki/src/components/registration/PublicIdentificationGrid.tsx
@@ -1,5 +1,6 @@
 import { Grid, Paper } from '@mui/material';
-import { H1, HeaderSeparator, Text } from 'shared/components';
+import { CustomButton, H1, HeaderSeparator, Text } from 'shared/components';
+import { Color, Variant } from 'shared/enums';
 import { useWindowProperties } from 'shared/hooks';
 
 import { SelectIdentificationMethod } from 'components/registration/identification/SelectIdentificationMethod';
@@ -8,7 +9,88 @@ import { PublicRegistrationExamSessionDetails } from 'components/registration/Pu
 import { PublicRegistrationStepper } from 'components/registration/PublicRegistrationStepper';
 import { usePublicTranslation } from 'configs/i18n';
 import { useAppSelector } from 'configs/redux';
+import { AppRoutes } from 'enums/app';
+import { ExamSession } from 'interfaces/examSessions';
 import { examSessionSelector } from 'redux/selectors/examSession';
+import { sessionSelector } from 'redux/selectors/session';
+
+const AlreadyLoggedIn = () => {
+  const { t } = usePublicTranslation({
+    keyPrefix: 'yki.component.registration.steps.identify.alreadyLoggedIn',
+  });
+  const { loggedInSession } = useAppSelector(sessionSelector);
+  const examSession = useAppSelector(examSessionSelector)
+    .examSession as ExamSession;
+  const isSuomiFiAuthenticatedSession =
+    loggedInSession?.['auth-method'] === 'SUOMIFI';
+  const isEmailAuthenticatedSession =
+    loggedInSession?.['auth-method'] === 'EMAIL';
+
+  return (
+    <>
+      <Text>
+        {t('caption')}
+        <br />
+        {isSuomiFiAuthenticatedSession && (
+          <>
+            <strong>{t('labels.name') + ':'}</strong>
+            &nbsp;
+            {`${loggedInSession.identity.first_name} ${loggedInSession.identity.last_name}`}
+          </>
+        )}
+        {isEmailAuthenticatedSession && (
+          <>
+            <strong>{t('labels.email') + ':'}</strong>
+            &nbsp;
+            {`${loggedInSession.identity['external-user-id']}`}
+          </>
+        )}
+      </Text>
+      <Text>{t('reauthenticate')}</Text>
+      <div className="rows gapped-xs align-items-center">
+        <CustomButton
+          aria-label={t('labels.continueToRegistration')}
+          variant={Variant.Contained}
+          color={Color.Secondary}
+          className="fit-content-max-width"
+          size="large"
+          href={AppRoutes.ExamSessionRegistration.replace(
+            /:examSessionId/,
+            `${examSession.id}`,
+          )}
+        >
+          {t('labels.continueToRegistration')}
+        </CustomButton>
+        <CustomButton
+          aria-label={t('labels.abort')}
+          variant={Variant.Text}
+          color={Color.Secondary}
+          className="fit-content-max-width"
+          size="large"
+          href={AppRoutes.Registration}
+        >
+          {t('labels.abort')}
+        </CustomButton>
+      </div>
+    </>
+  );
+};
+
+const Identify = () => {
+  const { t } = usePublicTranslation({
+    keyPrefix: 'yki.component.registration.steps.identify',
+  });
+
+  return (
+    <>
+      <Text>{t('registrationIsBindingAdvisory')}</Text>
+      <div className="gapped rows">
+        <SelectIdentificationMethod />
+        <PublicRegistrationControlButtons />
+      </div>
+    </>
+  );
+};
 
 export const PublicIdentificationGrid = () => {
   const { t } = usePublicTranslation({
@@ -17,6 +99,7 @@ export const PublicIdentificationGrid = () => {
   const { isPhone } = useWindowProperties();
 
   const { examSession } = useAppSelector(examSessionSelector);
+  const { loggedInSession } = useAppSelector(sessionSelector);
 
   if (!examSession) {
     return null;
@@ -34,7 +117,9 @@ export const PublicIdentificationGrid = () => {
           <div className="rows gapped-xxl">
             <PublicRegistrationStepper />
             <div className="rows public-registration__grid__heading">
-              <H1>{t('title')}</H1>
+              <H1>
+                {loggedInSession ? t('alreadyLoggedIn.title') : t('title')}
+              </H1>
               <HeaderSeparator />
             </div>
           </div>
@@ -45,11 +130,7 @@ export const PublicIdentificationGrid = () => {
                   examSession={examSession}
                   showOpenings={true}
                 />
-                <Text>{t('registrationIsBindingAdvisory')}</Text>
-                <div className="gapped rows">
-                  <SelectIdentificationMethod />
-                  <PublicRegistrationControlButtons />
-                </div>
+                {loggedInSession ? <AlreadyLoggedIn /> : <Identify />}
               </div>
             </div>
           </Paper>
