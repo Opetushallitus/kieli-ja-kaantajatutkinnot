e3db0c4cf86e03b5a754be3ef66a3c90eb3e803e
Pyry Koivisto <pyry.koivisto@mavericks.fi>
2024-03-07 10:30:56 +0200

    YKI(Frontend): By default return credentials for user authenticated through Suomi.fi

--- frontend/packages/yki/setupProxy.js
+++ frontend/packages/yki/setupProxy.js
@@ -269,6 +269,7 @@ const quarantines = {
 
 const initialOrganizers = [...organizers];
 
+/*
 const adminUser = {
   identity: {
     username: 'ykitestaaja',
@@ -283,6 +284,7 @@ const adminUser = {
   },
   'auth-method': 'CAS',
 };
+*/
 
 /*
 const organizerUser = {
@@ -310,7 +312,7 @@ const loginLinkAuthenticatedUser = {
 };
 */
 
-/*
+
 const suomiFiAuthenticatedUser = {
   identity: {
     first_name: 'Susanna',
@@ -326,7 +328,6 @@ const suomiFiAuthenticatedUser = {
   },
   'auth-method': 'SUOMIFI',
 };
-*/
 
 /*
 const unauthenticatedUser = {
@@ -1022,7 +1023,7 @@ module.exports = function (app) {
   app.get('/yki/api/user/identity', (req, res) => {
     try {
       res.set('Content-Type', 'application/json; charset=utf-8');
-      res.send(adminUser);
+      res.send(suomiFiAuthenticatedUser);
     } catch (err) {
       printError(req, err);
       res.status(404).send(err.message);
