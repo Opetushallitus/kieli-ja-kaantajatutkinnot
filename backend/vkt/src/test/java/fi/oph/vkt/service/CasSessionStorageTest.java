32ce6d35277fd5c0c032caef5c2295fdb9ca927e
Jarkko Pesonen <435495+jrkkp@users.noreply.github.com>
2024-05-03 10:08:01 +0300

    VKT(Backend): deploy commit [deploy]

--- backend/vkt/src/test/java/fi/oph/vkt/service/CasSessionStorageTest.java
+++ backend/vkt/src/test/java/fi/oph/vkt/service/CasSessionStorageTest.java
@@ -1,9 +1,5 @@
 package fi.oph.vkt.service;
 
-import static org.mockito.Mockito.mock;
-
-import fi.oph.vkt.service.auth.CasSessionMappingStorage;
-import jakarta.servlet.http.HttpSession;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.security.test.context.support.WithMockUser;
