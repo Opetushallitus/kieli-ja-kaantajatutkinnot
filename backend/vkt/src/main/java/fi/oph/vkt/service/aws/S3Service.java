d1df823c5d4c06402acd2f71830b68b7df768b18
Pyry Koivisto <pyry.koivisto@mavericks.fi>
2024-06-13 12:55:54 +0300

    VKT(Backend): Try to add session token to response if available [deploy]

--- backend/vkt/src/main/java/fi/oph/vkt/service/aws/S3Service.java
+++ backend/vkt/src/main/java/fi/oph/vkt/service/aws/S3Service.java
@@ -7,7 +7,12 @@ import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import software.amazon.awssdk.auth.credentials.AwsCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.AwsSessionCredentials;
 import software.amazon.awssdk.awscore.presigner.PresignedRequest;
 import software.amazon.awssdk.services.s3.model.GetObjectRequest;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
@@ -23,11 +28,13 @@ public class S3Service {
   private final S3Presigner presigner;
   private final S3PostObjectPresigner postObjectPresigner;
   private final S3Config s3Config;
+  private final AwsCredentialsProvider awsCredentialsProvider;
 
   private static final int MAX_SIZE_100_MB = 100 * 1024 * 1024;
   private static final String ACCEPTED_CONTENT_TYPES =
     "application/pdf,image/jpeg,image/png,image/heic,image/tiff,image/webp";
   private static final Duration POST_POLICY_VALID_FOR_ONE_MIN = Duration.ofMinutes(1);
+  private static final Logger LOG = LoggerFactory.getLogger(S3Service.class);
 
   public String getPresignedUrl(String key) {
     GetObjectRequest request = GetObjectRequest.builder().bucket(s3Config.getBucketName()).key(key).build();
@@ -59,6 +66,12 @@ public class S3Service {
     HashMap<String, String> presignedRequestWithKey = new HashMap<>(presigned);
     presignedRequestWithKey.put("key", key);
     presignedRequestWithKey.put("bucketURI", s3Config.getBucketURI());
+
+    AwsCredentials credentials = awsCredentialsProvider.resolveCredentials();
+    LOG.info("credentials: class {}", credentials.getClass().getName());
+    if (credentials instanceof AwsSessionCredentials) {
+      presignedRequestWithKey.put("x-amz-session-token", ((AwsSessionCredentials) credentials).sessionToken());
+    }
 
     return presignedRequestWithKey;
   }
