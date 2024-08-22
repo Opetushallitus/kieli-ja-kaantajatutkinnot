e1d2c70d85c7e8e561f981fd93c701d07a9f3aa6
Pyry Koivisto <pyry.koivisto@mavericks.fi>
2024-08-22 09:47:46 +0300

    AKR(Backend): Configure WebClient [deploy]

--- backend/akr/src/main/java/fi/oph/akr/config/AppConfig.java
+++ backend/akr/src/main/java/fi/oph/akr/config/AppConfig.java
@@ -9,6 +9,7 @@ import fi.oph.akr.service.email.sender.EmailSenderViestintapalvelu;
 import fi.vm.sade.javautils.nio.cas.CasClient;
 import fi.vm.sade.javautils.nio.cas.CasClientBuilder;
 import fi.vm.sade.javautils.nio.cas.CasConfig;
+import java.time.Duration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -16,9 +17,12 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.templatemode.TemplateMode;
+import reactor.netty.http.client.HttpClient;
+import reactor.netty.resources.ConnectionProvider;
 
 @Configuration
 public class AppConfig {
@@ -36,7 +40,9 @@ public class AppConfig {
   @ConditionalOnProperty(name = "app.email.sending-enabled", havingValue = "true")
   public EmailSender emailSender(@Value("${app.email.service-url}") final String emailServiceUrl) {
     LOG.info("emailServiceUrl: {}", emailServiceUrl);
-    final WebClient webClient = webClientBuilderWithCallerId().baseUrl(emailServiceUrl).build();
+    final WebClient webClient = webClientBuilderWithCallerId("email-sender-connection-provider")
+      .baseUrl(emailServiceUrl)
+      .build();
     return new EmailSenderViestintapalvelu(webClient, Constants.SERVICENAME, Constants.EMAIL_SENDER_NAME);
   }
 
@@ -79,7 +85,19 @@ public class AppConfig {
     return templateResolver;
   }
 
-  private static WebClient.Builder webClientBuilderWithCallerId() {
-    return WebClient.builder().defaultHeader("Caller-Id", Constants.CALLER_ID);
+  private static WebClient.Builder webClientBuilderWithCallerId(final String connectionProviderName) {
+    ConnectionProvider connectionProvider = ConnectionProvider
+      .builder(connectionProviderName)
+      .maxConnections(50)
+      .maxIdleTime(Duration.ofSeconds(20))
+      .maxLifeTime(Duration.ofSeconds(60))
+      .pendingAcquireTimeout(Duration.ofSeconds(60))
+      .evictInBackground(Duration.ofSeconds(120))
+      .build();
+    HttpClient httpClient = HttpClient.create(connectionProvider);
+    return WebClient
+      .builder()
+      .defaultHeader("Caller-Id", Constants.CALLER_ID)
+      .clientConnector(new ReactorClientHttpConnector(httpClient));
   }
 }
