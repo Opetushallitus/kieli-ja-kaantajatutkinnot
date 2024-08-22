b1569de83eae4a35d0e4085c157db553c61b98e8
Jarkko Pesonen <435495+jrkkp@users.noreply.github.com>
2024-08-22 15:17:44 +0300

    VKT(Backend) Possible fix for updated Spring error handling?

--- backend/vkt/src/main/java/fi/oph/vkt/api/IndexController.java
+++ backend/vkt/src/main/java/fi/oph/vkt/api/IndexController.java
@@ -1,6 +1,11 @@
 package fi.oph.vkt.api;
 
 import fi.oph.vkt.service.aws.S3Config;
+import fi.oph.vkt.util.exception.NotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 import jakarta.annotation.Resource;
 import jakarta.servlet.http.HttpServletResponse;
 import java.security.SecureRandom;
@@ -75,5 +80,11 @@ public class IndexController {
     final String cspNonce = getNonce();
     addCSPHeaders(response, cspNonce);
     return new ModelAndView("index.html", Map.of("cspNonce", cspNonce));
+  }
+
+  @ExceptionHandler(value = NoResourceFoundException.class)
+  @ResponseStatus(HttpStatus.NOT_FOUND)
+  public NotFoundException handleNoResourceFoundException(final NoResourceFoundException ex) {
+    return new NotFoundException("Not found");
   }
 }
