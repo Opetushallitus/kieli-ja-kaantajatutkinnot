016c0b648b891b10fa63129e88a7c2ccd3c33766
Jarkko Pesonen <435495+jrkkp@users.noreply.github.com>
2024-08-25 18:00:34 +0300

    VKT(Backend): More tests for KOSKI

--- backend/vkt/src/test/java/fi/oph/vkt/service/KoskiServiceTest.java
+++ backend/vkt/src/test/java/fi/oph/vkt/service/KoskiServiceTest.java
@@ -33,6 +33,9 @@ public class KoskiServiceTest {
   @Value("classpath:koski/koski-korkeakoulu-response.json")
   private org.springframework.core.io.Resource koskiKorkeakouluResponse;
 
+  @Value("classpath:koski/koski-nordea-demo-response.json")
+  private org.springframework.core.io.Resource koskiNordeaDemoResponse;
+
   private MockWebServer mockWebServer;
   private String koskiUrl;
 
@@ -59,6 +62,51 @@ public class KoskiServiceTest {
     assertEquals(1, educations.size());
   }
 
+  @Test
+  public void testFetchKoskiNordeaDemo() throws IOException, InterruptedException {
+    final List<PublicEducationDTO> educations = doRequest(getMockNordeaDemoResponse(), "1.2.246.562.24.37998958910");
+
+    final RecordedRequest request = mockWebServer.takeRequest();
+    assertEquals("POST", request.getMethod());
+    assertEquals(koskiUrl + "/oid", Objects.requireNonNull(request.getRequestUrl()).toString());
+    assertEquals("{\"oid\":\"1.2.246.562.24.37998958910\"}", request.getBody().readUtf8());
+    assertEquals(1, educations.size());
+  }
+
+  @Test
+  public void testFetchKoskiRetry() throws IOException, InterruptedException {
+    final WebClient webClient = WebClient.builder().baseUrl(koskiUrl).build();
+
+    mockWebServer.enqueue(
+      new MockResponse()
+        .setResponseCode(503)
+        .setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+        .setBody(
+          "[{\"key\":\"unavailable.virta\",\"message\":\"Korkeakoulutuksen opiskeluoikeuksia ei juuri nyt saada haettua. Yritä myöhemmin uudelleen.\"}]"
+        )
+    );
+    final String response = getMockKorkeakouluResponse();
+    mockWebServer.enqueue(
+      new MockResponse()
+        .setResponseCode(200)
+        .setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+        .setBody(response)
+    );
+
+    final KoskiService koskiService = new KoskiService(webClient);
+    final List<PublicEducationDTO> educations = koskiService.findEducations("1.2.246.562.24.97984579806");
+
+    final RecordedRequest failingRequest = mockWebServer.takeRequest();
+    assertEquals("POST", failingRequest.getMethod());
+    assertEquals(koskiUrl + "/oid", Objects.requireNonNull(failingRequest.getRequestUrl()).toString());
+
+    final RecordedRequest retryRequest = mockWebServer.takeRequest();
+    assertEquals("POST", retryRequest.getMethod());
+    assertEquals(koskiUrl + "/oid", Objects.requireNonNull(retryRequest.getRequestUrl()).toString());
+    assertEquals("{\"oid\":\"1.2.246.562.24.97984579806\"}", retryRequest.getBody().readUtf8());
+    assertEquals(1, educations.size());
+  }
+
   private List<PublicEducationDTO> doRequest(final String response, final String oid) {
     return doRequest(response, oid, 200);
   }
@@ -69,7 +117,7 @@ public class KoskiServiceTest {
     mockWebServer.enqueue(
       new MockResponse()
         .setResponseCode(responseCode)
-        .setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_XML_VALUE)
+        .setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
         .setBody(response)
     );
 
@@ -80,5 +128,9 @@ public class KoskiServiceTest {
 
   private String getMockKorkeakouluResponse() throws IOException {
     return new String(koskiKorkeakouluResponse.getInputStream().readAllBytes());
+  }
+
+  private String getMockNordeaDemoResponse() throws IOException {
+    return new String(koskiNordeaDemoResponse.getInputStream().readAllBytes());
   }
 }
