b6471063490278a1633f82e98516d99d4e81e19c
Pyry Koivisto <pyry.koivisto@mavericks.fi>
2024-09-18 12:45:30 +0300

    SHARED(Frontend): Introduce NavigationLinks component to replace use of MUI Tabs in header

---
+++ frontend/packages/shared/src/components/NavigationLinks/NavigationLinks.tsx
@@ -0,0 +1,33 @@
+import { Text } from '../Text/Text';
+import { Link } from 'react-router-dom';
+import './NavigationLinks.scss';
+
+interface NavigationLinksProps {
+  navigationAriaLabel: string;
+  links: Array<NavigationLink>;
+}
+
+interface NavigationLink {
+  active: boolean;
+  href: string;
+  label: string;
+}
+
+export const NavigationLinks = ({
+  navigationAriaLabel,
+  links,
+}: NavigationLinksProps) => {
+  return (
+    <nav className="navigation-links" aria-label={navigationAriaLabel}>
+      <ul>
+        {links.map((l, i) => (
+          <li key={i} className={l.active ? 'active' : undefined}>
+            <Link to={l.href} aria-current={l.active && 'page'}>
+              <Text>{l.label}</Text>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
