4f8f2b7dcaf0a98c49a29d6190ecb3c681bbacc8
Jarkko Pesonen <435495+jrkkp@users.noreply.github.com>
2024-09-30 11:18:21 +0300

    VKT(Backend): prettier commit [deploy]

--- backend/vkt/src/main/java/fi/oph/vkt/service/PublicEnrollmentService.java
+++ backend/vkt/src/main/java/fi/oph/vkt/service/PublicEnrollmentService.java
@@ -34,7 +34,6 @@ import fi.oph.vkt.util.PersonUtil;
 import fi.oph.vkt.util.exception.APIException;
 import fi.oph.vkt.util.exception.APIExceptionType;
 import fi.oph.vkt.util.exception.NotFoundException;
-
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
