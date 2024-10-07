9be24ea879b82b7f4ac9772a088685ad69860ede
Pyry Koivisto <pyry.koivisto@mavericks.fi>
2024-10-07 11:05:50 +0300

    SHARED(Frontend): Fix broken native select components

--- frontend/packages/shared/src/components/NativeSelect/NativeSelect.tsx
+++ frontend/packages/shared/src/components/NativeSelect/NativeSelect.tsx
@@ -11,12 +11,12 @@ import { ComboBoxOption } from '../../interfaces';
 import { Text } from '../Text/Text';
 
 export interface CustomNativeSelectProps
-  extends BaseSelectProps<ComboBoxOption> {
+  extends Omit<BaseSelectProps<string>, 'value'> {
   'data-testid'?: string;
   helperText?: string;
   showError?: boolean;
   placeholder: string;
-  value: '' | ComboBoxOption | undefined;
+  value: ComboBoxOption | undefined;
   values: Array<ComboBoxOption>;
 }
 
@@ -27,7 +27,7 @@ const CustomSelect = ({
   ...rest
 }: CustomNativeSelectProps) => {
   const options = [{ label: placeholder, value: '' }, ...values];
-  const inputValue = value && value.value ? value : '';
+  const inputValue = value && value.value ? value.value : '';
 
   return (
     <Select variant={Variant.Outlined} value={inputValue} {...rest} native>
