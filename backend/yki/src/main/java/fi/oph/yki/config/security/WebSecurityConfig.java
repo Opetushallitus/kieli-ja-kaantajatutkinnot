204f3ac794f6193227fb87cd6ad6137ede41dd14
Jarkko Pesonen <435495+jrkkp@users.noreply.github.com>
2024-11-04 10:06:36 +0200

    Snyk/updates 10 2024 (#748)
    * fix: upgrade axios from 1.7.4 to 1.7.7
    
    Snyk has created this PR to upgrade axios from 1.7.4 to 1.7.7.
    
    See this package in yarn:
    axios
    
    See this project in Snyk:
    https://app.snyk.io/org/oph-snyk/project/84abdeeb-9d9e-4376-92f5-0d0983809ad7?utm_source=github-cloud-app&utm_medium=referral&page=upgrade-pr
    
    * fix: upgrade org.liquibase:liquibase-core from 4.29.1 to 4.29.2
    
    Snyk has created this PR to upgrade org.liquibase:liquibase-core from 4.29.1 to 4.29.2.
    
    See this package in maven:
    org.liquibase:liquibase-core
    
    See this project in Snyk:
    https://app.snyk.io/org/oph-snyk/project/d824f7ed-bf61-4724-becd-c480f7e7c8ea?utm_source=github-cloud-app&utm_medium=referral&page=upgrade-pr
    
    * fix: upgrade io.netty:netty-resolver-dns-native-macos from 4.1.112.Final to 4.1.113.Final
    
    Snyk has created this PR to upgrade io.netty:netty-resolver-dns-native-macos from 4.1.112.Final to 4.1.113.Final.
    
    See this package in maven:
    io.netty:netty-resolver-dns-native-macos
    
    See this project in Snyk:
    https://app.snyk.io/org/oph-snyk/project/e59ac9f1-fa97-493f-be51-738241cda069?utm_source=github-cloud-app&utm_medium=referral&page=upgrade-pr
    
    * fix: upgrade react-router-dom from 6.26.1 to 6.26.2
    
    Snyk has created this PR to upgrade react-router-dom from 6.26.1 to 6.26.2.
    
    See this package in yarn:
    react-router-dom
    
    See this project in Snyk:
    https://app.snyk.io/org/oph-snyk/project/84abdeeb-9d9e-4376-92f5-0d0983809ad7?utm_source=github-cloud-app&utm_medium=referral&page=upgrade-pr
    
    * fix: backend/pom.xml to reduce vulnerabilities
    
    The following vulnerabilities are fixed with an upgrade:
    - https://snyk.io/vuln/SNYK-JAVA-ORGSPRINGFRAMEWORK-8230364
    - https://snyk.io/vuln/SNYK-JAVA-ORGSPRINGFRAMEWORK-8230365
    
    * fix: backend/pom.xml to reduce vulnerabilities
    
    The following vulnerabilities are fixed with an upgrade:
    - https://snyk.io/vuln/SNYK-JAVA-ORGSPRINGFRAMEWORK-8230364
    - https://snyk.io/vuln/SNYK-JAVA-ORGSPRINGFRAMEWORK-8230365
    - https://snyk.io/vuln/SNYK-JAVA-ORGSPRINGFRAMEWORK-8230366
    
    * fix: backend/pom.xml to reduce vulnerabilities
    
    The following vulnerabilities are fixed with an upgrade:
    - https://snyk.io/vuln/SNYK-JAVA-ORGSPRINGFRAMEWORK-8230372
    - https://snyk.io/vuln/SNYK-JAVA-ORGSPRINGFRAMEWORK-8230373
    - https://snyk.io/vuln/SNYK-JAVA-ORGSPRINGFRAMEWORK-8230364
    - https://snyk.io/vuln/SNYK-JAVA-ORGSPRINGFRAMEWORK-8230365
    - https://snyk.io/vuln/SNYK-JAVA-ORGSPRINGFRAMEWORK-8230366
    - https://snyk.io/vuln/SNYK-JAVA-ORGSPRINGFRAMEWORK-8230367
    - https://snyk.io/vuln/SNYK-JAVA-ORGSPRINGFRAMEWORK-8230368
    
    * fix: backend/pom.xml to reduce vulnerabilities
    
    The following vulnerabilities are fixed with an upgrade:
    - https://snyk.io/vuln/SNYK-JAVA-ORGSPRINGFRAMEWORK-8230372
    - https://snyk.io/vuln/SNYK-JAVA-ORGSPRINGFRAMEWORK-8230373
    - https://snyk.io/vuln/SNYK-JAVA-ORGSPRINGFRAMEWORK-8230364
    - https://snyk.io/vuln/SNYK-JAVA-ORGSPRINGFRAMEWORK-8230365
    - https://snyk.io/vuln/SNYK-JAVA-ORGSPRINGFRAMEWORK-8230366
    - https://snyk.io/vuln/SNYK-JAVA-ORGSPRINGFRAMEWORK-8230367
    - https://snyk.io/vuln/SNYK-JAVA-ORGSPRINGFRAMEWORK-8230368
    
    * fix: backend/pom.xml to reduce vulnerabilities
    
    The following vulnerabilities are fixed with an upgrade:
    - https://snyk.io/vuln/SNYK-JAVA-ORGSPRINGFRAMEWORKSECURITY-8309135
    - https://snyk.io/vuln/SNYK-JAVA-ORGSPRINGFRAMEWORK-8230372
    - https://snyk.io/vuln/SNYK-JAVA-ORGSPRINGFRAMEWORK-8230373
    - https://snyk.io/vuln/SNYK-JAVA-ORGSPRINGFRAMEWORK-8230364
    - https://snyk.io/vuln/SNYK-JAVA-ORGSPRINGFRAMEWORK-8230365
    - https://snyk.io/vuln/SNYK-JAVA-ORGSPRINGFRAMEWORK-8230366
    - https://snyk.io/vuln/SNYK-JAVA-ORGSPRINGFRAMEWORK-8230367
    - https://snyk.io/vuln/SNYK-JAVA-ORGSPRINGFRAMEWORK-8230368
    
    * VKT:AKR:OTR:YKI(Backend) Dummy commit for [deploy]
    
    * YKI(Backend) Disable default authentication [deploy]
    
    ---------
    
    Co-authored-by: snyk-io[bot] <141718529+snyk-io[bot]@users.noreply.github.com>

--- backend/yki/src/main/java/fi/oph/yki/config/security/WebSecurityConfig.java
+++ backend/yki/src/main/java/fi/oph/yki/config/security/WebSecurityConfig.java
@@ -2,6 +2,8 @@ package fi.oph.yki.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationServiceException;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -30,5 +32,12 @@ public class WebSecurityConfig {
     return httpSecurity.csrf(configurer ->
       configurer.csrfTokenRepository(csrfTokenRepository).csrfTokenRequestHandler(requestHandler)
     );
+  }
+
+  @Bean
+  public AuthenticationManager noopAuthenticationManager() {
+    return authentication -> {
+      throw new AuthenticationServiceException("Authentication is disabled");
+    };
   }
 }
