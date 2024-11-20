5ad7e0dbc18202ae3780113ffb02ee44036afb5c
Pyry Koivisto <pyry.koivisto@mavericks.fi>
2024-11-20 14:42:02 +0200

    VKT(Frontend): Texts for good and satisfactory level landing page [deploy]

--- frontend/packages/vkt/src/components/common/BulletList.tsx
+++ frontend/packages/vkt/src/components/common/BulletList.tsx
@@ -5,19 +5,27 @@ import { I18nNamespace } from 'shared/enums';
 import { BoldedTranslationString } from 'components/common/BoldedTranslationString';
 import { VktI18nNamespace } from 'configs/i18n';
 
+const renderBulletPoint = (
+  t: TFunction<I18nNamespace | VktI18nNamespace>,
+  key: string,
+) => <BoldedTranslationString i18nKey={key} t={t} />;
+
 export const BulletList = ({
   points,
   t,
+  renderListItem = renderBulletPoint,
 }: {
   points: Array<string>;
   t: TFunction<I18nNamespace | VktI18nNamespace, string>;
+  renderListItem?: (
+    t: TFunction<I18nNamespace | VktI18nNamespace>,
+    key: string,
+  ) => JSX.Element;
 }) => {
   return (
     <Typography className="margin-top-sm" variant="body1" component="ul">
       {points.map((point, i) => (
-        <li key={i}>
-          <BoldedTranslationString i18nKey={point} t={t} />
-        </li>
+        <li key={i}>{renderListItem(t, point)}</li>
       ))}
     </Typography>
   );
--- frontend/packages/vkt/src/pages/goodAndSatisfactoryLevel/PublicGoodAndSatisfactoryLevelLandingPage.tsx
+++ frontend/packages/vkt/src/pages/goodAndSatisfactoryLevel/PublicGoodAndSatisfactoryLevelLandingPage.tsx
@@ -1,11 +1,114 @@
-import { Box, Grid } from '@mui/material';
+import { Box, Container, Grid } from '@mui/material';
+import { TFunction } from 'i18next';
 import { FC, useEffect } from 'react';
-import { H1, HeaderSeparator, Text } from 'shared/components';
+import { Trans } from 'react-i18next';
+import { H1, H2, HeaderSeparator, Text } from 'shared/components';
+import { I18nNamespace } from 'shared/enums';
+import { useWindowProperties } from 'shared/hooks';
 
+import { BulletList } from 'components/common/BulletList';
 import { PublicExaminerListing } from 'components/publicExaminerListing/PublicExaminerListing';
-import { usePublicTranslation } from 'configs/i18n';
+import {
+  useCommonTranslation,
+  usePublicTranslation,
+  VktI18nNamespace,
+} from 'configs/i18n';
 import { useAppDispatch } from 'configs/redux';
 import { loadPublicExaminers } from 'redux/reducers/publicExaminer';
+
+const GeneralInformationBox = () => {
+  const { t } = usePublicTranslation({
+    keyPrefix: 'vkt.component.goodAndSatisfactoryLevel.infoBox.general',
+  });
+  const translateCommon = useCommonTranslation();
+
+  return (
+    <Container className="public-homepage__info-box">
+      <div className="rows gapped">
+        <H2>{t('title')}</H2>
+        <div className="rows">
+          <Text>{t('skills')}</Text>
+          <BulletList
+            t={t}
+            points={[
+              'bulletPoints.point1',
+              'bulletPoints.point2',
+              'bulletPoints.point3',
+            ]}
+          />
+        </div>
+
+        <Text>{translateCommon('info.selectExam')}</Text>
+        <Text>
+          <Trans
+            t={translateCommon}
+            i18nKey="info.previousEnrollment"
+            components={[<b key="0" />, <b key="1" />]}
+          ></Trans>
+        </Text>
+      </div>
+    </Container>
+  );
+};
+
+const BulletPointWithItalics = ({
+  i18nKey,
+  t,
+}: {
+  i18nKey: string;
+  t: TFunction<I18nNamespace | VktI18nNamespace>;
+}) => {
+  return <Trans i18nKey={i18nKey} t={t} components={[<i key={i18nKey} />]} />;
+};
+
+const EnrollmentBox = () => {
+  const { t } = usePublicTranslation({
+    keyPrefix: 'vkt.component.goodAndSatisfactoryLevel.infoBox.enrollment',
+  });
+
+  return (
+    <Container className="public-homepage__info-box">
+      <div className="rows gapped-sm">
+        <H2>{t('title')}</H2>
+        <BulletList
+          t={t}
+          renderListItem={(
+            t: TFunction<I18nNamespace | VktI18nNamespace>,
+            k: string,
+          ) => <BulletPointWithItalics i18nKey={k} t={t} />}
+          points={[
+            'bulletPoints.point1',
+            'bulletPoints.point2',
+            'bulletPoints.point3',
+            'bulletPoints.point4',
+            'bulletPoints.point5',
+            'bulletPoints.point6',
+            'bulletPoints.point7',
+            'bulletPoints.point8',
+          ]}
+        />
+      </div>
+    </Container>
+  );
+};
+
+const EnrollmentFeesBox = () => {
+  const { t } = usePublicTranslation({
+    keyPrefix: 'vkt.component.goodAndSatisfactoryLevel.infoBox.enrollmentFees',
+  });
+
+  return (
+    <Container className="public-homepage__info-box">
+      <div className="rows gapped-sm">
+        <H2>{t('title')}</H2>
+        <BulletList
+          t={t}
+          points={['bulletPoints.point1', 'bulletPoints.point2']}
+        />
+      </div>
+    </Container>
+  );
+};
 
 export const PublicGoodAndSatisfactoryLevelLandingPage: FC = () => {
   const { t } = usePublicTranslation({
@@ -15,6 +118,7 @@ export const PublicGoodAndSatisfactoryLevelLandingPage: FC = () => {
   useEffect(() => {
     dispatch(loadPublicExaminers());
   }, [dispatch]);
+  const { isPhone } = useWindowProperties();
 
   return (
     <Box className="public-homepage">
@@ -25,13 +129,25 @@ export const PublicGoodAndSatisfactoryLevelLandingPage: FC = () => {
         className="public-homepage__grid-container"
       >
         <Grid item className="public-homepage__grid-container__item-header">
-          <H1 data-testid="public-homepage__title-heading">{t('title')}</H1>
-          <HeaderSeparator />
-          <Text>
-            Jotain infotekstiä hyvän ja tyydyttävän taidon tutkinnoista. <br />
-            Mahdollisesti lyhyet ohjeet yhteydenottoon liittyen. <br />
-            Kenties linkki OPH:n sivuille.
-          </Text>
+          <div className="rows gapped-xxl">
+            <div>
+              <H1 data-testid="public-homepage__title-heading">{t('title')}</H1>
+              <HeaderSeparator />
+              <Text>
+                {t('description.part1')} {t('description.part2')}{' '}
+                {t('description.part3')} {t('description.part4')}
+              </Text>
+            </div>
+            <div
+              className={
+                isPhone ? 'rows gapped' : 'columns gapped align-items-start'
+              }
+            >
+              <GeneralInformationBox />
+              <EnrollmentBox />
+              <EnrollmentFeesBox />
+            </div>
+          </div>
         </Grid>
         <Grid item className="public-homepage__grid-container__result-box">
           <PublicExaminerListing />
