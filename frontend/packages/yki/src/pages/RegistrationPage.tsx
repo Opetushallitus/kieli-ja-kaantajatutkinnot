57ba20cf66221d92259abe07840fe00b36ede393
Laura Ketola <laura.ketola@gofore.com>
2024-12-03 13:13:40 +0200

    YKI(Frontend): Add temporary info box about terms changes [deploy]

--- frontend/packages/yki/src/pages/RegistrationPage.tsx
+++ frontend/packages/yki/src/pages/RegistrationPage.tsx
@@ -1,5 +1,5 @@
 import OpenInNewIcon from '@mui/icons-material/OpenInNew';
-import { Alert, Box, Grid, Link, Paper } from '@mui/material';
+import { Alert, Box, Container, Grid, Link, Paper } from '@mui/material';
 import { FC, useEffect, useState } from 'react';
 import { H1, H2, HeaderSeparator, Text } from 'shared/components';
 import { APIResponseStatus, Severity } from 'shared/enums';
@@ -15,6 +15,25 @@ import {
   selectFilteredPublicExamSessions,
 } from 'redux/selectors/examSessions';
 
+const InformationBox = () => {
+  const { t } = usePublicTranslation({
+    keyPrefix: 'yki.pages.registrationPage.infoBox',
+  });
+
+  return (
+    <Container className="public-registration-page__info-box">
+      <div>
+        <Text>{t('text')}:</Text>
+        <span className="columns gapped-xxs" style={{ display: 'inline-flex' }}>
+          <Link href={t('link')} target="_blank">
+            <Text>{t('linkText')}</Text>
+          </Link>
+          <OpenInNewIcon />
+        </span>
+      </div>
+    </Container>
+  );
+};
 export const RegistrationPage: FC = () => {
   const { t } = usePublicTranslation({
     keyPrefix: 'yki.pages.registrationPage',
@@ -59,6 +78,7 @@ export const RegistrationPage: FC = () => {
           item
           className="public-registration-page__grid-container__item-header"
         >
+          <InformationBox />
           <H1 data-testid="public-registration-page__title-heading">
             {t('title')}
           </H1>
