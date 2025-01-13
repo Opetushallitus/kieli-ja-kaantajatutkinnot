d61dffffa83acd6419e02b8743cd0efb6025b394
Pyry Koivisto <pyry.koivisto@mavericks.fi>
2025-01-13 21:50:44 +0200

    AKR:OTR:VKT:YKI(Frontend): Fix Jest setup after msw update broke it

--- frontend/packages/vkt/jest-environment-patched-jsdom.js
+++ frontend/packages/vkt/jest-environment-patched-jsdom.js
@@ -9,5 +9,7 @@ module.exports = class CustomTestEnvironment extends JSDOMEnvironment {
         this.global.TextDecoder = TextDecoder;
         this.global.Response = Response;
         this.global.Request = Request;
+        this.global.TransformStream = TransformStream;
+        this.global.BroadcastChannel = BroadcastChannel;
     }
 };
