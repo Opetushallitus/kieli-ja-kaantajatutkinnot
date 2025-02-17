c1636b3e06c1eec2c53632661018bcd6a884a445
Jarkko Pesonen <435495+jrkkp@users.noreply.github.com>
2025-02-17 10:44:26 +0200

    VKT(Backend) Fix request timeout data type for latest version

--- backend/vkt/src/main/java/fi/oph/vkt/service/onr/OnrOperationApiImpl.java
+++ backend/vkt/src/main/java/fi/oph/vkt/service/onr/OnrOperationApiImpl.java
@@ -6,11 +6,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import fi.oph.vkt.config.Constants;
 import fi.oph.vkt.util.HetuUtils;
 import fi.vm.sade.javautils.nio.cas.CasClient;
+
 import java.time.Duration;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
 import net.minidev.json.JSONArray;
 import org.asynchttpclient.Request;
 import org.asynchttpclient.RequestBuilder;
@@ -118,6 +118,6 @@ public class OnrOperationApiImpl implements OnrOperationApi {
       .addHeader("Accept", MediaType.APPLICATION_JSON_VALUE)
       .addHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
       .addHeader("Caller-Id", Constants.CALLER_ID)
-      .setRequestTimeout(Duration.ofMinutes(2));
+      .setRequestTimeout((int) Duration.ofMinutes(2).toMillis());
   }
 }
