3119ec9507566e284951528be50fc15e51de551a
Laura Ketola <52139145+lket@users.noreply.github.com>
2025-02-17 10:30:13 +0200

    Snyk updates (#776)
    * fix: upgrade org.liquibase:liquibase-core from 4.29.2 to 4.30.0
    
    Snyk has created this PR to upgrade org.liquibase:liquibase-core from 4.29.2 to 4.30.0.
    
    See this package in maven:
    org.liquibase:liquibase-core
    
    See this project in Snyk:
    https://app.snyk.io/org/oph-snyk/project/4612977b-f69e-4d1b-bf98-ce8f01ee9fbf?utm_source=github-cloud-app&utm_medium=referral&page=upgrade-pr
    
    * fix: upgrade com.fasterxml.jackson.dataformat:jackson-dataformat-xml from 2.17.2 to 2.18.1
    
    Snyk has created this PR to upgrade com.fasterxml.jackson.dataformat:jackson-dataformat-xml from 2.17.2 to 2.18.1.
    
    See this package in maven:
    com.fasterxml.jackson.dataformat:jackson-dataformat-xml
    
    See this project in Snyk:
    https://app.snyk.io/org/oph-snyk/project/e59ac9f1-fa97-493f-be51-738241cda069?utm_source=github-cloud-app&utm_medium=referral&page=upgrade-pr
    
    * fix: upgrade commons-io:commons-io from 2.16.1 to 2.18.0
    
    Snyk has created this PR to upgrade commons-io:commons-io from 2.16.1 to 2.18.0.
    
    See this package in maven:
    commons-io:commons-io
    
    See this project in Snyk:
    https://app.snyk.io/org/oph-snyk/project/e59ac9f1-fa97-493f-be51-738241cda069?utm_source=github-cloud-app&utm_medium=referral&page=upgrade-pr
    
    * fix: upgrade i18next from 23.16.4 to 23.16.8
    
    Snyk has created this PR to upgrade i18next from 23.16.4 to 23.16.8.
    
    See this package in yarn:
    i18next
    
    See this project in Snyk:
    https://app.snyk.io/org/oph-snyk/project/84abdeeb-9d9e-4376-92f5-0d0983809ad7?utm_source=github-cloud-app&utm_medium=referral&page=upgrade-pr
    
    * fix: upgrade react-router-dom from 6.27.0 to 6.28.0
    
    Snyk has created this PR to upgrade react-router-dom from 6.27.0 to 6.28.0.
    
    See this package in yarn:
    react-router-dom
    
    See this project in Snyk:
    https://app.snyk.io/org/oph-snyk/project/84abdeeb-9d9e-4376-92f5-0d0983809ad7?utm_source=github-cloud-app&utm_medium=referral&page=upgrade-pr
    
    * VKT:AKR:OTR:YKI(Frontend & Backend) maven + yarn updates [deploy]
    
    ---------
    
    Co-authored-by: snyk-io[bot] <141718529+snyk-io[bot]@users.noreply.github.com>

--- backend/akr/src/main/java/fi/oph/akr/onr/OnrOperationApiImpl.java
+++ backend/akr/src/main/java/fi/oph/akr/onr/OnrOperationApiImpl.java
@@ -10,6 +10,7 @@ import fi.oph.akr.onr.dto.PersonalDataDTO;
 import fi.oph.akr.onr.model.PersonalData;
 import fi.oph.akr.util.HetuUtils;
 import fi.vm.sade.javautils.nio.cas.CasClient;
+import java.time.Duration;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -214,6 +215,6 @@ public class OnrOperationApiImpl implements OnrOperationApi {
       .addHeader("Accept", MediaType.APPLICATION_JSON_VALUE)
       .addHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
       .addHeader("Caller-Id", Constants.CALLER_ID)
-      .setRequestTimeout((int) TimeUnit.MINUTES.toMillis(2));
+      .setRequestTimeout(Duration.ofMinutes(2));
   }
 }
