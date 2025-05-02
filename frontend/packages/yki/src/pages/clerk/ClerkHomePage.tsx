a9702edbc7114c2a8222fbf2e19c20edd774fddc
Kimmo Taskinen <kimmo.taskinen@knowit.fi>
2025-05-02 14:54:13 +0300

    Clerk UI init

---
+++ frontend/packages/yki/src/pages/clerk/ClerkHomePage.tsx
@@ -0,0 +1,24 @@
+import { Grid, Paper } from '@mui/material';
+import { FC } from 'react';
+import { H1, Text } from 'shared/components';
+
+export const ClerkHomePage: FC = () => {
+  return (
+    <div className="clerk-homepage">
+      <H1>Hello</H1>
+      <Paper elevation={3}>
+        <Grid
+          container
+          direction="column"
+          className="clerk-homepage__grid-container"
+        >
+          <Grid item>
+            <div className="columns gapped grow">
+              <Text>Uusi virkailijan UI</Text>
+            </div>
+          </Grid>
+        </Grid>
+      </Paper>
+    </div>
+  );
+};
