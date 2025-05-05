e10e42c4de3137766690c029dc55008a2c585944
Pyry Koivisto <pyry.koivisto@mavericks.fi>
2025-05-05 21:31:08 +0300

    YKI(Frontend): TransferEnrollmentPage first draft

--- frontend/packages/yki/src/routers/AppRouter.tsx
+++ frontend/packages/yki/src/routers/AppRouter.tsx
@@ -30,6 +30,7 @@ import { NotFoundPage } from 'pages/NotFoundPage';
 import { ReassessmentPage } from 'pages/ReassessmentPage';
 import { RegistrationPage } from 'pages/RegistrationPage';
 import { RegistrationPaymentStatusPage } from 'pages/RegistrationPaymentStatusPage';
+import { TransferEnrollmentPage } from 'pages/TransferEnrollmentPage';
 import { UserDetailsPage } from 'pages/UserDetailsPage';
 import { loadSession } from 'redux/reducers/session';
 import { sessionSelector } from 'redux/selectors/session';
@@ -150,6 +151,14 @@ export const AppRouter: FC = () => {
           element={
             <TitlePage title={createTitle('userDetails')}>
               <UserDetailsPage />
+            </TitlePage>
+          }
+        />
+        <Route
+          path={AppRoutes.TransferEnrollment}
+          element={
+            <TitlePage title={createTitle('transferEnrollment')}>
+              <TransferEnrollmentPage />
             </TitlePage>
           }
         />
