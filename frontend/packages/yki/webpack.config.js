1cc000572ca318da45156c19f7a4a7f706598dc6
Kimmo Taskinen <kimmo.taskinen@knowit.fi>
2025-05-07 09:43:13 +0300

    YKI(Frontend): Test with separate clerk build to target path yki/v2 [deploy]

--- frontend/packages/yki/webpack.config.js
+++ frontend/packages/yki/webpack.config.js
@@ -1,14 +1,24 @@
 const { merge } = require('webpack-merge');
 const common = require('../../webpack.common');
-
+const clerk = require('../../webpack.clerk');
 module.exports = (env) => {
-  const { getDefaults } = common(
-    'yki',
-    env,
-    __dirname,
-    4003,
-    'ilmoittautuminen'
-  );
-
-  return merge([getDefaults()]);
+  if (env.goal === 'clerk') {
+    const { getDefaults } = clerk(
+      'yki',
+      env,
+      __dirname,
+      4003,
+      'ilmoittautuminen'
+    );
+    return merge([getDefaults()]);
+  } else {
+    const { getDefaults } = common(
+      'yki',
+      env,
+      __dirname,
+      4003,
+      'ilmoittautuminen'
+    );
+    return merge([getDefaults()]);
+  }
 };
