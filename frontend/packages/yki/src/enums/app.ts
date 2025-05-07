a555624459e834588118f9d5b48a95c0cfa70eb5
Kimmo Taskinen <kimmo.taskinen@knowit.fi>
2025-05-07 10:12:40 +0300

    YKI(Frontend): Fix clerk url [deploy]

--- frontend/packages/yki/src/enums/app.ts
+++ frontend/packages/yki/src/enums/app.ts
@@ -5,7 +5,7 @@ export enum AppConstants {
 export enum AppRoutes {
   PublicRoot = '/yki',
   AccessibilityStatementPage = '/yki/saavutettavuus',
-  ClerkHomePage = '/yki/virkailija',
+  ClerkHomePage = '/yki/v2/virkailija',
   Registration = '/yki/ilmoittautuminen',
   RegistrationPaymentStatus = '/yki/ilmoittautuminen/maksu/tila',
   ExamSessionRegistration = '/yki/ilmoittautuminen/tutkintotilaisuus/:examSessionId',
