368d1f905447d1d045b8988a7ffe2c1ae6f30919
Pyry Koivisto <pyry.koivisto@mavericks.fi>
2025-05-14 14:56:31 +0300

    YKI(Frontend): Implement relocate functionality, add TransferEnrollmentSuccesPage [deploy]

--- frontend/packages/yki/setupProxy.js
+++ frontend/packages/yki/setupProxy.js
@@ -1459,7 +1459,7 @@ module.exports = function (app) {
         res.status(404).send(err.message);
       }
     };
-    useLocalProxy ? proxyPostCall(req, res) : mockCall();
+    useLocalProxy ? proxyDeleteCall(req, res) : mockCall();
   });
 
   app.get('/yki/api/person', (req, res) => {
@@ -1471,7 +1471,7 @@ module.exports = function (app) {
         res.status(404).send(err.message);
       }
     };
-    useLocalProxy ? proxyPostCall(req, res) : mockCall();
+    useLocalProxy ? proxyGetCall(req, res) : mockCall();
   });
 
   app.get('/yki/api/person/registration/:id/relocate', (req, res) => {
@@ -1491,6 +1491,31 @@ module.exports = function (app) {
               'Registration transfer details not found for registration id ' +
                 req.params.id,
             );
+        }
+      } catch (err) {
+        printError(req, err);
+        res.status(400).send(err.message);
+      }
+    };
+    useLocalProxy ? proxyGetCall(req, res) : mockCall();
+  });
+
+  app.post('/yki/api/person/registration/:id/relocate', (req, res) => {
+    const mockCall = () => {
+      try {
+        const registrationTransferDetails = getRegistrationTransferDetails();
+        const details = registrationTransferDetails.find(
+          (r) => r.id === Number(req.params.id),
+        );
+        res.set('Content-Type', 'application/json; charset=utf-8');
+        if (details) {
+          if (details.is_transferable) {
+            res.send({ success: true });
+          } else {
+            res.send({ success: false });
+          }
+        } else {
+          res.send({ success: false });
         }
       } catch (err) {
         printError(req, err);
--- frontend/packages/yki/src/redux/sagas/index.ts
+++ frontend/packages/yki/src/redux/sagas/index.ts
@@ -8,7 +8,7 @@ import { watchPublicIdentification } from 'redux/sagas/publicIdentification';
 import { watchRegistration } from 'redux/sagas/registration';
 import { watchReservationRequest } from 'redux/sagas/reservation';
 import { watchSession } from 'redux/sagas/session';
-import { watchTransferEnrollmentDetails } from 'redux/sagas/transferEnrollment';
+import { watchTransferEnrollment } from 'redux/sagas/transferEnrollment';
 import { watchUserDetails } from 'redux/sagas/userDetails';
 import { watchUserOpenRegistrations } from 'redux/sagas/userOpenRegistrations';
 
@@ -24,6 +24,6 @@ export default function* rootSaga() {
     watchSession(),
     watchUserDetails(),
     watchUserOpenRegistrations(),
-    watchTransferEnrollmentDetails(),
+    watchTransferEnrollment(),
   ]);
 }
