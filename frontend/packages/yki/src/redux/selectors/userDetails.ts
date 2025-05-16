1ad0a908f5ecfb236dca91f76004e0608f03af2b
Pyry Koivisto <pyry.koivisto@mavericks.fi>
2025-05-16 16:44:16 +0300

    YKI(Frontend): Improve styling of registration cards on UserDetailsPage, enrich PersonDetails with address details and phone number [deploy]

--- frontend/packages/yki/src/redux/selectors/userDetails.ts
+++ frontend/packages/yki/src/redux/selectors/userDetails.ts
@@ -1,3 +1,5 @@
 import { RootState } from 'configs/redux';
+import { UserDetailsState } from 'redux/reducers/userDetails';
 
-export const userDetailsSelector = (state: RootState) => state.userDetails;
+export const userDetailsSelector = (state: RootState): UserDetailsState =>
+  state.userDetails;
