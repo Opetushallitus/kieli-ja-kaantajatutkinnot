f2406b8256dcc15fdffbbe9401d2955a73798a4b
Kimmo Taskinen <kimmo.taskinen@knowit.fi>
2025-08-22 08:54:32 +0300

    YKI(Frontend): Added missing translations keys

--- frontend/packages/yki/src/components/registration/examSession/PublicExamSessionListing.tsx
+++ frontend/packages/yki/src/components/registration/examSession/PublicExamSessionListing.tsx
@@ -121,7 +121,7 @@ const RegistrationInitErrorModal = ({
     >
       <>
         <div className="rows gapped">
-          <H2>Paikan varaus epäonnistui</H2>
+          <H2>{t('title')}</H2>
           <Text id="registration-error-modal-description">
             {initRegistrationState.error ===
               PublicRegistrationInitError.ExamSessionFull && t('examIsFull')}
