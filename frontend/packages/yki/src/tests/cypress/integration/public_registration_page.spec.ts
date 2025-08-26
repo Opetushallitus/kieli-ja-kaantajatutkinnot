054abc54b58c4d155301ea85159ae1c55c85c1c6
Jarkko Pesonen <435495+jrkkp@users.noreply.github.com>
2025-08-26 17:31:54 +0300

    YKI(Frontend) Translation fixed in test assertion

--- frontend/packages/yki/src/tests/cypress/integration/public_registration_page.spec.ts
+++ frontend/packages/yki/src/tests/cypress/integration/public_registration_page.spec.ts
@@ -142,7 +142,7 @@ describe('PublicRegistrationPage', () => {
         .click();
 
       onPublicRegistrationPage.alertModalContains(
-        'Tilaisuus on täynnä. Voit ilmoittautua jonoon.',
+        'YKI-testi on täynnä. Voit ilmoittautua jonoon.'
       );
     });
 
