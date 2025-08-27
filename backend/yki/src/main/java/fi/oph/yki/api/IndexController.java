ea898f7efbe0a4897b12d1a787c6e4f1be62ea05
Jarkko Pesonen <435495+jrkkp@users.noreply.github.com>
2025-08-27 16:32:03 +0300

    Revert "YKI(Backend&Frontend): Take chatbot into use, try to relax CSP settings accordingly [deploy]"
    This reverts commit d2f298066a1cf6a9cf87bd20768881f584c183cb.

--- backend/yki/src/main/java/fi/oph/yki/api/IndexController.java
+++ backend/yki/src/main/java/fi/oph/yki/api/IndexController.java
@@ -19,13 +19,14 @@ public class IndexController {
   private static void addCSPHeaders(final HttpServletResponse response, final String nonce) {
     final String csp =
       "default-src 'none'; " +
-      "script-src 'self' cai-static.efectecloud.com 'nonce-" +
+      "script-src 'self' 'nonce-" +
       nonce +
       "' 'strict-dynamic' https: http: 'unsafe-inline'; " +
-      "style-src 'self' " +
-      "'unsafe-inline'; " +
-      "connect-src 'self' cai-static.efectecloud.com cai-rail.efectecloud.com;" +
-      "img-src 'self' cai-static.efectecloud.com; " +
+      "style-src 'self' 'nonce-" +
+      nonce +
+      "' 'unsafe-inline'; " +
+      "connect-src 'self'; " +
+      "img-src 'self'; " +
       "font-src 'self'; " +
       "base-uri 'self'; " +
       "form-action 'self';";
