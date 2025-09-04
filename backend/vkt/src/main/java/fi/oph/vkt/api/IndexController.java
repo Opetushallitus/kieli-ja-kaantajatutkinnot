20e299f30f0a3082304663fe0afb8ced036b3ddb
Jarkko Pesonen <435495+jrkkp@users.noreply.github.com>
2025-09-04 14:30:11 +0300

    VKT:AKR:OTR:YKI(Backend) Spring boot version bump [deploy]

--- backend/vkt/src/main/java/fi/oph/vkt/api/IndexController.java
+++ backend/vkt/src/main/java/fi/oph/vkt/api/IndexController.java
@@ -11,8 +11,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-// TODO: remove me, trigger CI
-
 @Controller
 @RequestMapping(value = "/")
 public class IndexController {
