3264bf4278d1fb635f0e9bf31d58ffecbc18268b
Pyry Koivisto <pyry.koivisto@mavericks.fi>
2025-10-10 18:13:11 +0300

    SHARED(Frontend): Add typing support for previously introduced custom typography variant 'label'

--- frontend/packages/shared/src/configs/materialUI.ts
+++ frontend/packages/shared/src/configs/materialUI.ts
@@ -241,3 +241,19 @@ export const theme = createTheme({
     },
   },
 });
+
+declare module '@mui/material/styles' {
+  interface TypographyVariants {
+    label: React.CSSProperties;
+  }
+
+  interface TypographyVariantsOptions {
+    label?: React.CSSProperties;
+  }
+}
+
+declare module '@mui/material/Typography' {
+  interface TypographyPropsVariantOverrides {
+    label: true;
+  }
+}
