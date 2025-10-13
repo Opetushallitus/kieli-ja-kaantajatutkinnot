bf01294202110ff3b7465196cb127462f0c70748
Saku K <6057704+saku-koodari@users.noreply.github.com>
2025-10-13 12:59:01 +0300

    YKI(Frontend): add Customer Details - component

--- frontend/packages/yki/src/pages/clerk/ClerkCustomerDetailsPage.tsx
+++ frontend/packages/yki/src/pages/clerk/ClerkCustomerDetailsPage.tsx
@@ -1,9 +1,45 @@
+import { ChevronRight, HomeOutlined } from '@mui/icons-material';
+import { Box, Grid, IconButton, Paper } from '@mui/material';
 import { FC } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { ClerkCustomerDetails } from 'components/customer/ClerkCustomerDetails';
+import { useCommonTranslation } from 'configs/i18n';
+import { AppRoutes } from 'enums/app';
+import { H2 } from 'ophTheme/Text';
 
 export const ClerkCustomerDetailsPage: FC = () => {
+  const translateCommon = useCommonTranslation();
+
+  const navigate = useNavigate();
+
   return (
-    <>
-      <h2>Henkilö XYZ</h2>
-    </>
+    <Box className="clerk-customer-details-page">
+      <div className="columns gapped-xs">
+        <IconButton
+          color="secondary"
+          className="clerk-customer-details-page__home-button"
+          onClick={() => navigate(AppRoutes.CustomerSearch)}
+        >
+          <HomeOutlined color="secondary" fontSize="large" />
+        </IconButton>
+        <ChevronRight color="disabled" fontSize="large" />
+        <H2>{translateCommon('loadingContent')}</H2>
+      </div>
+
+      <Grid
+        container
+        rowSpacing={4}
+        direction="column"
+        className="clerk-customer-details-page__grid-container"
+      >
+        <Paper
+          elevation={3}
+          className="customer-details-page__grid-container__results"
+        >
+          <ClerkCustomerDetails />
+        </Paper>
+      </Grid>
+    </Box>
   );
 };
