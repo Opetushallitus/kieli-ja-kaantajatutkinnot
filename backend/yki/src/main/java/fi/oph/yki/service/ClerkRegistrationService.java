4345caaac45f04d7da236718dfb16c9be8949f6d
Jarkko Pesonen <435495+jrkkp@users.noreply.github.com>
2025-10-15 16:40:04 +0300

    YKI(Backend) Try different logger settings [deploy]

--- backend/yki/src/main/java/fi/oph/yki/service/ClerkRegistrationService.java
+++ backend/yki/src/main/java/fi/oph/yki/service/ClerkRegistrationService.java
@@ -130,7 +130,7 @@ public class ClerkRegistrationService {
       .registration(clerkRegistrationDTO)
       .examSession(examSessionDTO)
       .status(registration.getState())
-      .languageOfCommunication(RegistrationLangOfCommunication.FI) // TODO
+      .languageOfCommunication(RegistrationLangOfCommunication.FI) // TODO, deploy
       .freeRegistrationBasis(freeRegistration.getType())
       .freeRegistrationsLeft(countFreeRegistrationsLeft(freeRegistration))
       .attachments(createClerkApprovalAttachmentsDTO(freeRegistration)) // TODO
