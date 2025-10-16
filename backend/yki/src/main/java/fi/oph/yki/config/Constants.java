cfdef6d4dae8177b1cf47c0ad4e4cd3dd9998139
Pyry Koivisto <pyry.koivisto@mavericks.fi>
2025-10-16 16:57:38 +0300

    YKI(Backend): Fix APP_ADMIN_ROLE to use existing role [deploy]

--- backend/yki/src/main/java/fi/oph/yki/config/Constants.java
+++ backend/yki/src/main/java/fi/oph/yki/config/Constants.java
@@ -4,6 +4,6 @@ public class Constants {
 
   public static final String CALLER_ID = "1.2.246.562.10.00000000001.yki";
   public static final String SERVICENAME = "yki";
-  public static final String APP_ADMIN_ROLE = "APP_YKI_PAAKAYTTAJA";
+  public static final String APP_ADMIN_ROLE = "APP_YKI_YLLAPITAJA";
   public static final String EMAIL_SENDER_NAME = "Yleiset kielitutkinnot | Opetushallitus";
 }
