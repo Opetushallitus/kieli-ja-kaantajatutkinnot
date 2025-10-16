a3a5cd7ccced011bc667cca7dd1630b939eef05a
Saku K <6057704+saku-koodari@users.noreply.github.com>
2025-10-16 11:12:57 +0300

    YKI(Frontend): Fix tests

--- frontend/packages/yki/src/tests/cypress/support/page-objects/clerkCustomerDetailsPage.ts
+++ frontend/packages/yki/src/tests/cypress/support/page-objects/clerkCustomerDetailsPage.ts
@@ -2,7 +2,11 @@ import { customerDetails } from 'tests/msw/fixtures/customerDetails';
 
 class ClerkCustomerDetailsPage {
   elements = {
-    title: (id: number) => cy.findByText(customerDetails[id - 1].person.name),
+    title: (id: number) => {
+      const person = customerDetails[id - 1].person;
+
+      return cy.findByText(`${person.lastName} ${person.firstName}`);
+    },
   };
   isVisible(id: number) {
     this.elements.title(id).should('be.visible');
