0fff40bda2f6bf0032d943a3685f99004b517aad
Pyry Koivisto <pyry.koivisto@mavericks.fi>
2025-10-20 09:20:54 +0300

    YKI(Backend): Include user OID in audit log messages [deploy]

--- backend/yki/src/main/java/fi/oph/yki/audit/AuditUtil.java
+++ backend/yki/src/main/java/fi/oph/yki/audit/AuditUtil.java
@@ -30,10 +30,9 @@ public class AuditUtil {
     final InetAddress inetAddress = getInetAddress(request);
     final String session = request.getSession().getId();
     final String userAgent = request.getHeader("User-Agent");
-    return new User(inetAddress, session, userAgent);
-    // return getOptionalOid()
-    //   .map(oid -> new User(oid, inetAddress, session, userAgent))
-    //   .orElseGet(() -> new User(inetAddress, session, userAgent));
+    return getOptionalOid()
+       .map(oid -> new User(oid, inetAddress, session, userAgent))
+       .orElseGet(() -> new User(inetAddress, session, userAgent));
   }
 
   private static InetAddress getInetAddress(final HttpServletRequest request) {
