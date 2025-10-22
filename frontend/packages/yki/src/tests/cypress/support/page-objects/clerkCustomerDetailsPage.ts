260ef206472fa39dbc6fc43222ccaf0a40479eed
Saku K <6057704+saku-koodari@users.noreply.github.com>
2025-10-22 18:39:43 +0300

    test exams data

--- frontend/packages/yki/src/tests/cypress/support/page-objects/clerkCustomerDetailsPage.ts
+++ frontend/packages/yki/src/tests/cypress/support/page-objects/clerkCustomerDetailsPage.ts
@@ -42,6 +42,42 @@ class ClerkCustomerDetailsPage {
       .should('have.attr', 'href')
       .and('include', `mailto:${details.person.email}`);
   }
+
+  // Ilmoittautumiset
+  expectRegisteredTableHeadersVisible() {
+    cy.findAllByText('Ilmoittautumiset').should('be.visible');
+    const registeredTableHeader = this.elements.registeredTableHeader;
+    registeredTableHeader().eq(0).should('have.text', 'Tutkintopäivä');
+    registeredTableHeader().eq(1).should('have.text', 'Tutkinto');
+    registeredTableHeader().eq(2).should('have.text', 'Testipaikka');
+    registeredTableHeader().eq(3).should('have.text', 'Ilmoittautumisen tila');
+    registeredTableHeader().eq(4).should('have.text', 'Ilmoittautumispvm');
+    registeredTableHeader().should('have.length', 5); // check no extra columans
+  }
+
+  // Jonossa
+  expectedQueuedTableHeadersVisible() {
+    cy.findAllByText('Jonossa').should('be.visible');
+    const queuedTableHeader = this.elements.queuedTableHeader;
+    queuedTableHeader().eq(0).should('have.text', 'Tutkintopäivä');
+    queuedTableHeader().eq(1).should('have.text', 'Tutkinto');
+    queuedTableHeader().eq(2).should('have.text', 'Testipaikka');
+    queuedTableHeader().eq(3).should('have.text', 'Ilmoittautumisen tila');
+    queuedTableHeader().eq(4).should('have.text', 'Ilmoittautumispvm');
+    queuedTableHeader().eq(5).should('have.text', 'Jonopaikkaa tarjottu');
+    queuedTableHeader().should('have.length', 6); // check no extra columns
+  }
+
+  // Menneet tutkintotilaisuudet
+  expectPastTableHeadersVisible() {
+    cy.findAllByText('Menneet tutkintotilaisuudet').should('be.visible');
+    const pastTableHeader = this.elements.pastTableHeader;
+    pastTableHeader().eq(0).should('have.text', 'Tutkintopäivä');
+    pastTableHeader().eq(1).should('have.text', 'Tutkinto');
+    pastTableHeader().eq(2).should('have.text', 'Testipaikka');
+    pastTableHeader().eq(3).should('have.text', 'Tila');
+    pastTableHeader().should('have.length', 4); // check no extra columns
+  }
 }
 
 export const onClerkCustomerDetailsPage = new ClerkCustomerDetailsPage();
