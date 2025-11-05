08da06a04a3f225e8d1a752ed31b363533d6583a
Saku Kaarakainen <6057704+saku-koodari@users.noreply.github.com>
2025-11-05 13:30:10 +0200

    Apply suggestion from @jrkkp
    Co-authored-by: Jarkko Pesonen <435495+jrkkp@users.noreply.github.com>

--- backend/yki/src/main/java/fi/oph/yki/api/clerk/ClerkCustomerDetailsController.java
+++ backend/yki/src/main/java/fi/oph/yki/api/clerk/ClerkCustomerDetailsController.java
@@ -8,6 +8,8 @@ import fi.oph.yki.service.ClerkCustomerService;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.annotation.Resource;
 import java.util.List;
+
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/v2/api/clerk/customer", consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
+@Profile("dev")
 public class ClerkCustomerDetailsController {
 
   @Resource
