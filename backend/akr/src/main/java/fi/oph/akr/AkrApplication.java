330168fdfd17f63f64f1a9107288d79d40cac4a9
Jarkko Pesonen <435495+jrkkp@users.noreply.github.com>
2025-11-12 11:43:57 +0200

    YKI:VKT:AKR:OTR(Backend) Trigger CI

--- backend/akr/src/main/java/fi/oph/akr/AkrApplication.java
+++ backend/akr/src/main/java/fi/oph/akr/AkrApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AkrApplication {
 
+  // Trigger CI. Remove this
   public static void main(final String[] args) {
     SpringApplication.run(AkrApplication.class, args);
   }
