faea756616c32044c90150720d01001ac02ce884
Jarkko Pesonen <435495+jrkkp@users.noreply.github.com>
2025-11-17 11:21:48 +0200

    AKR:VKT:OTR(Backend) Trigger CI commit

--- backend/otr/src/main/java/fi/oph/otr/OtrApplication.java
+++ backend/otr/src/main/java/fi/oph/otr/OtrApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OtrApplication {
 
+  // Trigger CI. Remove this
   public static void main(String[] args) {
     SpringApplication.run(OtrApplication.class, args);
   }
