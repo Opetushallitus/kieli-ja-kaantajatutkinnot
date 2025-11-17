c517259538277466d32176d3324940ca60d7b760
Jarkko Pesonen <435495+jrkkp@users.noreply.github.com>
2025-11-17 11:29:44 +0200

    VKT(Backend) Prettier

--- backend/vkt/src/main/java/fi/oph/vkt/repository/EnrollmentAppointmentRepository.java
+++ backend/vkt/src/main/java/fi/oph/vkt/repository/EnrollmentAppointmentRepository.java
@@ -3,7 +3,6 @@ package fi.oph.vkt.repository;
 import fi.oph.vkt.model.EnrollmentAppointment;
 import fi.oph.vkt.model.Examiner;
 import fi.oph.vkt.model.Person;
-
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.Query;
