e6db7604ca5e2b8c6ae0b24c143db2ffab139abf
Jarkko Pesonen <435495+jrkkp@users.noreply.github.com>
2025-11-24 12:36:07 +0200

    AKR:OTR(Backend) Remove comment

--- backend/otr/src/main/java/fi/oph/otr/OtrApplication.java
+++ backend/otr/src/main/java/fi/oph/otr/OtrApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OtrApplication {
 
-  // Deploy commit
   public static void main(String[] args) {
     SpringApplication.run(OtrApplication.class, args);
   }
