65ab273ba7142748c93cf4d505703a54097fbbce
Kimmo Taskinen <kimmo.taskinen@knowit.fi>
2025-12-08 08:50:28 +0200

    YKI(Frontend): Use new list-table-row to manage collapsible content

--- frontend/packages/yki/src/components/clerkRegister/listing/ClerkRegisterListing.tsx
+++ frontend/packages/yki/src/components/clerkRegister/listing/ClerkRegisterListing.tsx
@@ -49,6 +49,11 @@ export const ClerkRegisterListing = ({
   const filteredOrganizers = useAppSelector(selectFilteredClerkOrganizers);
   const rows = filteredOrganizers.map((organizer) => ({
     ...organizer,
+    collapsibleContent: {
+      name: organizer.contact_name,
+      email: organizer.contact_email,
+      phone: organizer.contact_phone_numner,
+    },
   }));
 
   const pagination = {
