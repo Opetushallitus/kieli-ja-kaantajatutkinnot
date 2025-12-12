39a780c5d82c0241adf5ba8aebddf6b7474583d4
Kimmo Taskinen <kimmo.taskinen@knowit.fi>
2025-12-12 14:16:59 +0200

    YKI(Frontend): Get both organization and organizer details + examSessions for CollapsibleRow

--- frontend/packages/yki/src/components/clerkRegister/listing/ClerkRegisterListing.tsx
+++ frontend/packages/yki/src/components/clerkRegister/listing/ClerkRegisterListing.tsx
@@ -1,24 +1,22 @@
 import { Error } from '@mui/icons-material';
 import { Collapse, TableCell, TableRow, Typography } from '@mui/material';
 import { Box } from '@mui/system';
-import { Dayjs } from 'dayjs';
+import dayjs, { Dayjs } from 'dayjs';
 import i18next from 'i18next';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { CustomCircularProgress } from 'shared/components';
 import { APIResponseStatus, Color } from 'shared/enums';
 import { DateUtils } from 'shared/utils';
 
 import { ListTable } from 'components/oph-design/table/list-table';
 import { ListTableColumn } from 'components/oph-design/table/table-types';
+import axiosInstance from 'configs/axios';
 import { usePublicTranslation } from 'configs/i18n';
 import { useAppDispatch, useAppSelector } from 'configs/redux';
 import { OrganizerLanguage } from 'interfaces/clerkOrganizer';
 import { Label, Text } from 'ophTheme/Text';
-import { loadClerkOrganizers } from 'redux/reducers/clerkOrganizer';
-import {
-  clerkOrganizersSelector,
-  selectFilteredClerkOrganizers,
-} from 'redux/selectors/clerkOrganizers';
+import { loadClerkOrganizerRegistry } from 'redux/reducers/clerkOrganizer';
+import { clerkOrganizersSelector } from 'redux/selectors/clerkOrganizers';
 import {
   getLanguagesWithLevelDescriptions,
   languagesToString,
@@ -45,10 +43,12 @@ export const ClerkRegisterListing = ({
   page,
   setPage,
 }: ClerkRegisterListingProps) => {
-  const { status } = useAppSelector(clerkOrganizersSelector);
-  const filteredOrganizers = useAppSelector(selectFilteredClerkOrganizers);
-  const rows = filteredOrganizers.map((organizer) => ({
-    ...organizer,
+  const { organizerRegistryStatus, organizerRegistry } = useAppSelector(
+    clerkOrganizersSelector,
+  );
+
+  const rows = organizerRegistry.map((organizer) => ({
+    ...organizer.organizer,
   }));
 
   const pagination = {
@@ -99,13 +99,14 @@ export const ClerkRegisterListing = ({
   ];
 
   const dispatch = useAppDispatch();
-  useEffect(() => {
-    if (status === APIResponseStatus.NotStarted) {
-      dispatch(loadClerkOrganizers());
-    }
-  }, [dispatch, status]);
 
-  switch (status) {
+  useEffect(() => {
+    if (organizerRegistryStatus === APIResponseStatus.NotStarted) {
+      dispatch(loadClerkOrganizerRegistry());
+    }
+  }, [dispatch, organizerRegistryStatus]);
+
+  switch (organizerRegistryStatus) {
     case APIResponseStatus.NotStarted:
     case APIResponseStatus.InProgress:
       return <CustomCircularProgress color={Color.Secondary} />;
@@ -146,54 +147,74 @@ const ClerkRegisterCollapsibleRow = ({
   row: ClerkOrganizerType;
   open: boolean;
   t: typeof i18next.t;
-}) => (
-  <TableRow>
-    <TableCell
-      style={{ height: 'unset', paddingTop: 0, paddingBottom: 0 }}
-      colSpan={3}
-    >
-      <Collapse in={open} timeout="auto" unmountOnExit>
-        <Box sx={{ margin: '1rem 4rem' }}>
-          <div
-            style={{
-              display: 'flex',
-              flexDirection: 'row',
-              gap: '0.5rem',
-              justifyContent: 'space-between',
-            }}
-          >
-            <div className="rows">
-              <Label>{t('organizerAgreement')}</Label>
-              <Text>
-                {`${DateUtils.formatOptionalDate(
-                  row.agreement_start_date,
-                )} - ${DateUtils.formatOptionalDate(row.agreement_end_date)}`}
-              </Text>{' '}
-            </div>
+}) => {
+  const [_examSessions, setExamSessions] = useState([]);
 
-            <div className="rows">
-              <Label>{t('languageProficiencies')}</Label>
-              {getLanguagesWithLevelDescriptions(row.languages || []).map(
-                (lang) => (
-                  <Text key={lang}>{lang}</Text>
-                ),
-              )}
+  useEffect(() => {
+    if (open) {
+      const fetchExamSessions = async () => {
+        const oneYearAgo = dayjs().subtract(1, 'year').format('YYYY-MM-DD');
+        const response = await axiosInstance.get(
+          `/yki/api/clerk/organizer/${row.oid}/exam-session`,
+          { params: { from: oneYearAgo } },
+        );
+        setExamSessions(response.data);
+      };
+      fetchExamSessions();
+    }
+  }, [row.oid, open, setExamSessions]);
+
+  return (
+    <TableRow>
+      <TableCell
+        style={{ height: 'unset', paddingTop: 0, paddingBottom: 0 }}
+        colSpan={3}
+      >
+        <Collapse in={open} timeout="auto" unmountOnExit>
+          <Box sx={{ margin: '1rem 4rem' }}>
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'row',
+                gap: '0.5rem',
+                justifyContent: 'space-between',
+              }}
+            >
+              <div className="rows">
+                <Label>{t('organizerAgreement')}</Label>
+                <Text>
+                  {`${DateUtils.formatOptionalDate(
+                    row.agreement_start_date,
+                  )} - ${DateUtils.formatOptionalDate(row.agreement_end_date)}`}
+                </Text>{' '}
+              </div>
+
+              <div className="rows">
+                <Label>{t('languageProficiencies')}</Label>
+                {getLanguagesWithLevelDescriptions(row.languages || []).map(
+                  (lang) => (
+                    <Text key={lang}>{lang}</Text>
+                  ),
+                )}
+              </div>
+              <div className="rows">
+                <Label>{t('contactInfo')}</Label>
+                <Text>{row.contact_name}</Text>
+                <Text>{row.contact_phone_number}</Text>
+                <Text>
+                  <a href={`mailto:${row.contact_email}`}>
+                    {row.contact_email}
+                  </a>
+                </Text>
+              </div>
+              <div className="rows">
+                <Label>{t('extraInfo')}</Label>
+                <Text>{row.extra}</Text>
+              </div>
             </div>
-            <div className="rows">
-              <Label>{t('contactInfo')}</Label>
-              <Text>{row.contact_name}</Text>
-              <Text>{row.contact_phone_number}</Text>
-              <Text>
-                <a href={`mailto:${row.contact_email}`}>{row.contact_email}</a>
-              </Text>
-            </div>
-            <div className="rows">
-              <Label>{t('extraInfo')}</Label>
-              <Text>{row.extra}</Text>
-            </div>
-          </div>
-        </Box>
-      </Collapse>
-    </TableCell>
-  </TableRow>
-);
+          </Box>
+        </Collapse>
+      </TableCell>
+    </TableRow>
+  );
+};
--- frontend/packages/yki/src/redux/reducers/clerkOrganizer.ts
+++ frontend/packages/yki/src/redux/reducers/clerkOrganizer.ts
@@ -2,15 +2,20 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { APIResponseStatus } from 'shared/enums';
 
 import { ClerkOrganizer } from 'interfaces/clerkOrganizer';
+import { ClerkOrganizerRegistry } from 'interfaces/clerkOrganizerRegistry';
 
 interface ClerkOrganizerState {
   organizers: Array<ClerkOrganizer>;
+  organizerRegistry: Array<ClerkOrganizerRegistry>;
   status: APIResponseStatus;
+  organizerRegistryStatus?: APIResponseStatus;
 }
 
 const initialState: ClerkOrganizerState = {
   organizers: [],
+  organizerRegistry: [],
   status: APIResponseStatus.NotStarted,
+  organizerRegistryStatus: APIResponseStatus.NotStarted,
 };
 
 const clerkOrganizersSlice = createSlice({
@@ -27,6 +32,19 @@ const clerkOrganizersSlice = createSlice({
       state.status = APIResponseStatus.Success;
       state.organizers = action.payload;
     },
+    loadClerkOrganizerRegistry(state) {
+      state.organizerRegistryStatus = APIResponseStatus.InProgress;
+    },
+    rejectClerkOrganizerRegistry(state) {
+      state.organizerRegistryStatus = APIResponseStatus.Error;
+    },
+    storeClerkOrganizerRegistry(
+      state,
+      action: PayloadAction<Array<ClerkOrganizerRegistry>>,
+    ) {
+      state.organizerRegistryStatus = APIResponseStatus.Success;
+      state.organizerRegistry = action.payload;
+    },
   },
 });
 
@@ -35,4 +53,7 @@ export const {
   loadClerkOrganizers,
   rejectClerkOrganizers,
   storeClerkOrganizers,
+  loadClerkOrganizerRegistry,
+  rejectClerkOrganizerRegistry,
+  storeClerkOrganizerRegistry,
 } = clerkOrganizersSlice.actions;
