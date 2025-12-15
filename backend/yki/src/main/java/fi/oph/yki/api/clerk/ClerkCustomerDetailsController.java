092e26ae29a86a47e89fb193784bdf48df8fccc9
Saku K <6057704+saku-koodari@users.noreply.github.com>
2025-12-15 15:52:52 +0200

    YKI(Backend): API to search all customers

--- backend/yki/src/main/java/fi/oph/yki/api/clerk/ClerkCustomerDetailsController.java
+++ backend/yki/src/main/java/fi/oph/yki/api/clerk/ClerkCustomerDetailsController.java
@@ -9,10 +9,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import jakarta.annotation.Resource;
 import java.util.List;
 import org.springframework.context.annotation.Profile;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/v2/api/clerk/customer", consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
@@ -23,6 +20,12 @@ public class ClerkCustomerDetailsController {
   private ClerkCustomerService service;
 
   private static final String TAG_CUSTOMER = "Clerk customer API";
+
+  @PostMapping(path = "/search", consumes = ALL_VALUE)
+  @Operation(tags = TAG_CUSTOMER, summary = "Search customers")
+  public List<ClerkCustomerDetailsDTO> searchCustomers() throws Exception {
+    return service.searchClerkCustomers();
+  }
 
   @GetMapping(path = "/{oid}", consumes = ALL_VALUE)
   @Operation(tags = TAG_CUSTOMER, summary = "Get customer details")
--- backend/yki/src/main/java/fi/oph/yki/service/ClerkCustomerService.java
+++ backend/yki/src/main/java/fi/oph/yki/service/ClerkCustomerService.java
@@ -3,6 +3,7 @@ package fi.oph.yki.service;
 import fi.oph.yki.api.dto.clerk.*;
 import fi.oph.yki.model.ExamPayment;
 import fi.oph.yki.model.Person;
+import fi.oph.yki.model.Registration;
 import fi.oph.yki.onr.OnrService;
 import fi.oph.yki.onr.dto.PersonalDataDTO;
 import fi.oph.yki.repository.PersonRepository;
@@ -12,6 +13,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -24,84 +26,104 @@ public class ClerkCustomerService {
   private final RegistrationRepository registrationRepository;
   private final OnrService onrService;
 
-  private ClerkCustomerPersonDTO getClerkCustomerPersonDTO(String oid) throws Exception {
-    Person person = personRepository.getByOid(oid);
-    if (person == null) {
-      // throw 404, because the whole data of getClerkCustomerDetails is tied to a specific user.
-      throw new NotFoundException(String.format("Person with oid '%s' not found from the person repository.", oid));
-    }
-
-    PersonalDataDTO onrPerson = onrService.getPersonalData(oid);
-    if (onrPerson == null) {
-      throw new RuntimeException(
+  private ClerkCustomerPersonDTO PersonToDTO(Person person) throws NotFoundException {
+    try {
+      final var oid = person.getOid();
+      final var onrPerson = Objects.requireNonNull(
+        onrService.getPersonalData(oid),
         String.format("Person with oid '%s' was found in the person repository, ", oid) +
         "but not found from the oppijanumerorekisteri-service."
       );
+
+      return new ClerkCustomerPersonDTO(
+        person.getFirstName(),
+        person.getLastName(),
+        onrPerson.getIdentityNumber(),
+        person.getOid(),
+        person.getNationalityCode(),
+        person.getPhoneNumber(),
+        person.getAddress(),
+        person.getEmail()
+      );
+    } catch (Exception e) {
+      throw new NotFoundException(e.getMessage());
     }
-    return new ClerkCustomerPersonDTO(
-      person.getFirstName(),
-      person.getLastName(),
-      onrPerson.getIdentityNumber(),
-      person.getOid(),
-      person.getNationalityCode(),
-      person.getPhoneNumber(),
-      person.getAddress(),
-      person.getEmail()
+  }
+
+  private ClerkCustomerRegistrationDTO RegistrationToDTO(Registration registration) {
+    final var session = registration.getExamSession();
+
+    final var examDate = session.getExamDate().getExamDate();
+    final var exam = new ClerkExamDTO(session.getLanguage(), session.getLevel());
+
+    final var examLocation = session
+      .getLocations()
+      .stream()
+      .map(l -> new ClerkExamLocationDTO(l.getName(), l.getPostOffice(), l.getLang()))
+      .toList();
+
+    final var latestExamPayment = registration
+      .getExamPayments()
+      .stream()
+      .max(Comparator.comparing(ExamPayment::getPaidAt));
+
+    final Optional<LocalDate> freeRegistrationCreatedAt = registration.getFreeRegistration() == null
+      ? Optional.empty()
+      : Optional.of(registration.getFreeRegistration().getCreatedAt().toLocalDate());
+
+    final var paidAt = latestExamPayment.map(ExamPayment::getPaidAt).map(LocalDateTime::toLocalDate);
+    final var registrationDate = paidAt.or(() -> freeRegistrationCreatedAt);
+
+    final Optional<LocalDate> liftedFromQueueAt = registration.getLiftedFromQueueAt() == null
+      ? Optional.empty()
+      : Optional.of(registration.getLiftedFromQueueAt().toLocalDate());
+
+    final Optional<LocalDate> expiresAt = registration.getExpiresAt() == null
+      ? Optional.empty()
+      : Optional.of(registration.getExpiresAt().toLocalDate());
+
+    return new ClerkCustomerRegistrationDTO(
+      examDate,
+      exam,
+      examLocation,
+      registration.getState(),
+      paidAt,
+      registrationDate,
+      registration.getKind(),
+      liftedFromQueueAt,
+      expiresAt
     );
   }
 
-  private List<ClerkCustomerRegistrationDTO> getClerkCustomerRegistrationDTOs(String oid) throws Exception {
-    return registrationRepository
-      .getByPersonOid(oid)
-      .stream()
-      .map(registration -> {
-        final var session = registration.getExamSession();
+  private ClerkCustomerPersonDTO getClerkCustomerPersonDTO(String oid) throws Exception {
+    return PersonToDTO(
+      Objects.requireNonNull(
+        personRepository.getByOid(oid),
+        // throw 404, because the whole data of getClerkCustomerDetails is tied to a specific user.
+        String.format("Person with oid '%s' not found from the person repository.", oid)
+      )
+    );
+  }
 
-        final var examDate = session.getExamDate().getExamDate();
-        final var exam = new ClerkExamDTO(session.getLanguage(), session.getLevel());
-
-        final var examLocation = session
-          .getLocations()
-          .stream()
-          .map(l -> new ClerkExamLocationDTO(l.getName(), l.getPostOffice(), l.getLang()))
-          .toList();
-
-        final var latestExamPayment = registration
-          .getExamPayments()
-          .stream()
-          .max(Comparator.comparing(ExamPayment::getPaidAt));
-
-        final Optional<LocalDate> freeRegistrationCreatedAt = registration.getFreeRegistration() == null
-          ? Optional.empty()
-          : Optional.of(registration.getFreeRegistration().getCreatedAt().toLocalDate());
-
-        final var paidAt = latestExamPayment.map(ExamPayment::getPaidAt).map(LocalDateTime::toLocalDate);
-        final var registrationDate = paidAt.or(() -> freeRegistrationCreatedAt);
-
-        final Optional<LocalDate> liftedFromQueueAt = registration.getLiftedFromQueueAt() == null
-          ? Optional.empty()
-          : Optional.of(registration.getLiftedFromQueueAt().toLocalDate());
-
-        final Optional<LocalDate> expiresAt = registration.getExpiresAt() == null
-          ? Optional.empty()
-          : Optional.of(registration.getExpiresAt().toLocalDate());
-
-        return new ClerkCustomerRegistrationDTO(
-          examDate,
-          exam,
-          examLocation,
-          registration.getState(),
-          paidAt,
-          registrationDate,
-          registration.getKind(),
-          liftedFromQueueAt,
-          expiresAt
-        );
-      })
-      .toList();
+  private List<ClerkCustomerRegistrationDTO> getClerkCustomerRegistrationDTOs(String oid) {
+    return registrationRepository.getByPersonOid(oid).stream().map(this::RegistrationToDTO).toList();
   }
 
   public ClerkCustomerDetailsDTO getClerkCustomerDetails(String oid) throws Exception {
     return new ClerkCustomerDetailsDTO(getClerkCustomerPersonDTO(oid), getClerkCustomerRegistrationDTOs(oid));
+  }
+
+  public List<ClerkCustomerDetailsDTO> searchClerkCustomers() throws Exception {
+    final var persons = personRepository.findAll();
+    return persons
+      .stream()
+      .map(person -> {
+        var registrations = registrationRepository.getByPersonOid(person.getOid());
+        return new ClerkCustomerDetailsDTO(
+          PersonToDTO(person),
+          registrations.stream().map(this::RegistrationToDTO).toList()
+        );
+      })
+      .toList();
   }
 }
