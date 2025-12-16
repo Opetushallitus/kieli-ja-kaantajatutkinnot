5c20a8d4a94a908d85f37c1419cdda4a6a4ab295
Pyry Koivisto <pyry.koivisto@mavericks.fi>
2025-12-16 17:13:30 +0200

    YKI(Backend): Require different role for accessing integration endpoint

--- backend/yki/src/main/java/fi/oph/yki/config/Constants.java
+++ backend/yki/src/main/java/fi/oph/yki/config/Constants.java
@@ -5,5 +5,7 @@ public class Constants {
   public static final String CALLER_ID = "1.2.246.562.10.00000000001.yki";
   public static final String SERVICENAME = "yki";
   public static final String APP_ADMIN_ROLE = "APP_YKI_YLLAPITAJA";
+  // TODO Create new role
+  public static final String APP_EVALUATION_UPDATER_ROLE = "APP_YKI_ARVIOINNIN_TILAN_KIRJOITUS";
   public static final String EMAIL_SENDER_NAME = "Yleiset kielitutkinnot | Opetushallitus";
 }
