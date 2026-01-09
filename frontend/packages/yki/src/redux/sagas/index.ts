4ed1065e4677c6ed750b8f63494cb4826c275442
Jarkko Pesonen <435495+jrkkp@users.noreply.github.com>
2026-01-09 16:10:55 +0200

    YKI(Frontend) Lint fixes

--- frontend/packages/yki/src/redux/sagas/index.ts
+++ frontend/packages/yki/src/redux/sagas/index.ts
@@ -1,8 +1,8 @@
 import { all } from 'redux-saga/effects';
 
 import { watchClerkCustomerDetails } from 'redux/sagas/clerkCustomerDetails';
-import { watchClerkExamSession } from 'redux/sagas/clerkExamSession';
 import { watchClerkCustomersSearch } from 'redux/sagas/clerkCustomersSearch';
+import { watchClerkExamSession } from 'redux/sagas/clerkExamSession';
 import { watchClerkFreeRegistrations } from 'redux/sagas/clerkFreeRegistration';
 import { watchClerkFreeRegistrationDetails } from 'redux/sagas/clerkFreeRegistrationDetails';
 import { watchClerkOrganizers } from 'redux/sagas/clerkOrganizer';
