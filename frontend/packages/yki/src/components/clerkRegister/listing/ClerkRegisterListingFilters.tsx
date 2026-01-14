8fe8025a88df78028137a905c9141f2fa8a5acd5
Kimmo Taskinen <kimmo.taskinen@knowit.fi>
2026-01-14 10:05:57 +0200

    YKI(Frontend): Use OphSelectFormField. Added search icon to search box

--- frontend/packages/yki/src/components/clerkRegister/listing/ClerkRegisterListingFilters.tsx
+++ frontend/packages/yki/src/components/clerkRegister/listing/ClerkRegisterListingFilters.tsx
@@ -1,5 +1,9 @@
-import { FormControl, InputLabel, MenuItem, Select } from '@mui/material';
-import { OphInputFormField } from '@opetushallitus/oph-design-system';
+import { Search } from '@mui/icons-material';
+import { IconButton, InputAdornment } from '@mui/material';
+import {
+  OphInputFormField,
+  OphSelectFormField,
+} from '@opetushallitus/oph-design-system';
 
 import { usePublicTranslation } from 'configs/i18n';
 import { useAppDispatch, useAppSelector } from 'configs/redux';
@@ -17,12 +21,6 @@ export const ClerkRegisterListingFilters = () => {
   const searchQuery = useAppSelector(
     (state) => state.clerkOrganizer.searchQuery,
   );
-  const languageFilter = useAppSelector(
-    (state) => state.clerkOrganizer.languageFilter,
-  );
-  const levelFilter = useAppSelector(
-    (state) => state.clerkOrganizer.levelFilter,
-  );
 
   const { t } = usePublicTranslation({
     keyPrefix: 'yki.component.clerkRegister',
@@ -35,57 +33,61 @@ export const ClerkRegisterListingFilters = () => {
       className="columns gapped"
       style={{ marginBottom: '2rem', alignItems: 'flex-start' }}
     >
-      <div style={{ flex: 1 }}>
+      <div style={{ flex: 1, maxWidth: '400px' }}>
         <OphInputFormField
+          sx={{ width: '100%' }}
           label={t('listing.filters.searchLabel')}
           value={searchQuery}
           onChange={(e) => dispatch(setSearchQuery(e.target.value))}
-          fullWidth
+          endAdornment={
+            <InputAdornment position="end">
+              <IconButton
+                sx={{ padding: 0, margin: '0 4px' }}
+                aria-label={t('listing.filters.searchLabel')}
+                onClick={() => dispatch(setSearchQuery(searchQuery))}
+                onMouseDown={(e) => e.preventDefault()}
+                onMouseUp={(e) => e.preventDefault()}
+                edge="end"
+              >
+                <Search color="disabled" fontSize="large" />
+              </IconButton>
+            </InputAdornment>
+          }
         />
       </div>
-      <div style={{ minWidth: '200px' }}>
-        <FormControl fullWidth>
-          <InputLabel id="language-filter-label">
-            {t('listing.filters.languageLabel')}
-          </InputLabel>
-          <Select
-            labelId="language-filter-label"
-            label={t('listing.filters.languageLabel')}
-            value={languageFilter}
-            onChange={(e) => dispatch(setLanguageFilter(e.target.value))}
-          >
-            <MenuItem value="">
-              <em>{t('listing.filters.all')}</em>
-            </MenuItem>
-            {LANGUAGES.map((lang) => (
-              <MenuItem key={lang.code} value={lang.code}>
-                {lang.name}
-              </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
+      <div style={{ minWidth: '150px' }}>
+        <OphSelectFormField
+          sx={{ width: '100%' }}
+          label={t('listing.filters.languageLabel')}
+          inputProps={{
+            'aria-label': t('listing.filters.languageLabel'),
+          }}
+          options={[
+            { label: t('listing.filters.all'), value: '' },
+            ...LANGUAGES.map((lang) => ({
+              label: lang.name,
+              value: lang.code,
+            })),
+          ]}
+          onChange={(e) => dispatch(setLanguageFilter(e.target.value))}
+        />
       </div>
-      <div style={{ minWidth: '200px' }}>
-        <FormControl fullWidth>
-          <InputLabel id="level-filter-label">
-            {t('listing.filters.levelLabel')}
-          </InputLabel>
-          <Select
-            labelId="level-filter-label"
-            label={t('listing.filters.levelLabel')}
-            value={levelFilter}
-            onChange={(e) => dispatch(setLevelFilter(e.target.value))}
-          >
-            <MenuItem value="">
-              <em>{t('listing.filters.all')}</em>
-            </MenuItem>
-            {levels.map((level) => (
-              <MenuItem key={level} value={level}>
-                {levelDescription(level)}
-              </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
+      <div style={{ minWidth: '150px' }}>
+        <OphSelectFormField
+          sx={{ width: '100%' }}
+          label={t('listing.filters.levelLabel')}
+          inputProps={{
+            'aria-label': t('listing.filters.languageLabel'),
+          }}
+          options={[
+            { label: t('listing.filters.all'), value: '' },
+            ...levels.map((level) => ({
+              label: levelDescription(level),
+              value: level,
+            })),
+          ]}
+          onChange={(e) => dispatch(setLevelFilter(e.target.value))}
+        />
       </div>
     </div>
   );
