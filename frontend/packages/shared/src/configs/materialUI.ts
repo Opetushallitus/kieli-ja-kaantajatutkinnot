3437ca1db83131f3334b7e3eb541313372abe356
Kimmo Taskinen <kimmo.taskinen@knowit.fi>
2026-03-05 15:05:40 +0200

    YKI(Frontend): Updated how theme is defined for YKI Public

--- frontend/packages/shared/src/configs/materialUI.ts
+++ frontend/packages/shared/src/configs/materialUI.ts
@@ -4,8 +4,7 @@ import type {} from '@mui/x-date-pickers/themeAugmentation';
 const colorPrimary = '#ffffff';
 const colorTextPrimary = '#000a48';
 const colorSecondaryDark = '#000a48';
-// const colorSecondary = '#0041dc';
-const colorSecondary = '#378703';
+const colorSecondary = '#0041dc';
 const colorSecondaryLight = '#159ecb'; // not in figma specs
 
 const colorGrey200 = '#f5f5f5';
