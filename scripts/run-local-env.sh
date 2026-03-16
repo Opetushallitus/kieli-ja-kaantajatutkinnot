237104cc3aaa5ad1863a51aab01c5074880bfd1b
Saku K <6057704+saku-koodari@users.noreply.github.com>
2026-03-16 14:46:54 +0200

    Show error message if tmux or tmuxinator is not installed

--- scripts/run-local-env.sh
+++ scripts/run-local-env.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 
+if ! command -v tmux &>/dev/null; then
+  echo "tmux is not installed" >&2
+  exit 1
+fi
+
+if ! command -v tmuxinator &>/dev/null; then
+  echo "tmuxinator is not installed" >&2
+  exit 1
+fi
+
 tmuxinator start -p $(dirname "${BASH_SOURCE[0]}")/kios-tmux.yml
