0a763928a3fa684c06b6ed2bf4210befbb38215f
Kimmo Taskinen <kimmo.taskinen@knowit.fi>
2026-03-25 09:38:02 +0200

    YKI:SHARED(Frontend & Backend): Updated toast error text + backend lint [deploy]

--- backend/yki/src/test/java/fi/oph/yki/api/clerk/ClerkExamDateControllerTest.java
+++ backend/yki/src/test/java/fi/oph/yki/api/clerk/ClerkExamDateControllerTest.java
@@ -11,10 +11,10 @@ import net.minidev.json.JSONArray;
 import net.minidev.json.JSONObject;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(ClerkExamDateController.class)
