a6097aa7c24d94194a40fd293b64a2d1c2cc6ae2
kimmotaskinen-knowit <kimmo.taskinen@knowit.fi>
2026-03-31 09:54:10 +0300

    YKI(Frontend & Backend): Tutkintopäivän poisto med tarkistusarviointi [deploy]

--- backend/yki/src/main/java/fi/oph/yki/repository/EvaluationRepository.java
+++ backend/yki/src/main/java/fi/oph/yki/repository/EvaluationRepository.java
@@ -1,6 +1,7 @@
 package fi.oph.yki.repository;
 
 import fi.oph.yki.model.Evaluation;
+import fi.oph.yki.model.ExamDate;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
