c86d9994d5bd07aada9a60dd6986e131ba9789ce
Saku K <6057704+saku-koodari@users.noreply.github.com>
2026-04-07 18:51:11 +0300

    YKI(Frontend): Add sub-tabs for Osallistumiskiellot - page

--- frontend/packages/yki/clerk/src/components/clerkQuarantine/ClerkQuarantine.tsx
+++ frontend/packages/yki/clerk/src/components/clerkQuarantine/ClerkQuarantine.tsx
@@ -1,5 +1,74 @@
-import { FC } from 'react';
+import { Divider } from '@mui/material';
+import { Dispatch, SetStateAction, useState } from 'react';
 
-export const ClerkQuarantine: FC = () => {
-  return <></>;
+import { ClerkQuarantineListing } from 'components/clerkQuarantine/ClerkQuarantineListing';
+import { usePublicTranslation } from 'configs/i18n';
+
+const TABS = ['pending', 'previous', 'active'] as const;
+type Tab = (typeof TABS)[number];
+
+type QuarantineTabsProps = {
+  activeTab: Tab;
+  setActiveTab: Dispatch<SetStateAction<Tab>>;
+  setPage: Dispatch<SetStateAction<number>>;
+};
+
+const QuarantineTabs = ({
+  activeTab,
+  setActiveTab,
+  setPage,
+}: QuarantineTabsProps) => {
+  const { t } = usePublicTranslation({
+    keyPrefix: 'yki.component.clerkQuarantine.tabs',
+  });
+
+  const handleTabChange = (tab: Tab) => {
+    setActiveTab(tab);
+    setPage(1);
+  };
+
+  return (
+    <div className="clerk-quarantine__filter-tabs">
+      <div className="columns gapped">
+        {TABS.map((tab) => (
+          <div
+            key={tab}
+            className={`clerk-quarantine__filter-tabs__tab ${
+              activeTab === tab ? 'active' : ''
+            }`}
+            onClick={() => handleTabChange(tab)}
+            role="button"
+            tabIndex={0}
+            onKeyDown={() => handleTabChange(tab)}
+          >
+            {t(tab)}
+          </div>
+        ))}
+      </div>
+      <Divider />
+    </div>
+  );
+};
+
+export const ClerkQuarantine = () => {
+  const [activeTab, setActiveTab] = useState<Tab>('pending');
+  const [page, setPage] = useState(1);
+  const [pageSize, setPageSize] = useState(10);
+
+  return (
+    <div className="rows gapped">
+      <QuarantineTabs
+        activeTab={activeTab}
+        setActiveTab={setActiveTab}
+        setPage={setPage}
+      />
+      <ClerkQuarantineListing
+        page={page}
+        setPage={setPage}
+        pageSize={pageSize}
+        setPageSize={setPageSize}
+        activeTab={activeTab}
+      />
+    </div>
+  );
 };
---
+++ frontend/packages/yki/clerk/src/components/clerkQuarantine/ClerkQuarantineListing.tsx
@@ -0,0 +1,11 @@
+type ClerkQuarantineListingProps = {
+  page: number;
+  setPage: (page: number) => void;
+  pageSize: number;
+  setPageSize: (pageSize: number) => void;
+  activeTab: 'pending' | 'previous' | 'active';
+};
+
+export const ClerkQuarantineListing = ({}: ClerkQuarantineListingProps) => {
+  return <></>;
+};
