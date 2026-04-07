08014f803898db20d68046a703dbb5f043c5fbc6
Saku K <6057704+saku-koodari@users.noreply.github.com>
2026-04-07 18:30:16 +0300

    YKI(Frontend): Make Quarantine page - file. Adds title to the page.

--- frontend/packages/yki/clerk/src/pages/ClerkQuarantinePage.tsx
+++ frontend/packages/yki/clerk/src/pages/ClerkQuarantinePage.tsx
@@ -1,5 +1,30 @@
+import { Box, Grid, Paper, Typography } from '@mui/material';
 import { FC } from 'react';
 
+import { ClerkQuarantine } from 'components/clerkQuarantine/ClerkQuarantine';
+import { usePublicTranslation } from 'configs/i18n';
+
 export const ClerkQuarantinePage: FC = () => {
-  return <></>;
+  const { t } = usePublicTranslation({
+    keyPrefix: 'yki.pages.clerkQuarantinePage',
+  });
+
+  return (
+    <Box className="clerk-quarantine-page">
+      <Typography variant="h2">{t('heading')}</Typography>
+      <Grid
+        container
+        rowSpacing={4}
+        direction="column"
+        className="clerk-quarantine-page__grid-container"
+      >
+        <Paper
+          elevation={3}
+          className="clerk-quarantine-page__grid-container__results"
+        >
+          <ClerkQuarantine />
+        </Paper>
+      </Grid>
+    </Box>
+  );
 };
