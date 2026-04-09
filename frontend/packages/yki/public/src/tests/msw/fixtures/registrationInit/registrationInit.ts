befcf40b4f2e096a1f1239996313fdc18d8188d5
Kimmo Taskinen <kimmo.taskinen@knowit.fi>
2026-04-09 14:18:55 +0300

    YKI(Frontend): Fixed incorrect type [deploy]

--- frontend/packages/yki/public/src/tests/msw/fixtures/registrationInit/registrationInit.ts
+++ frontend/packages/yki/public/src/tests/msw/fixtures/registrationInit/registrationInit.ts
@@ -3,7 +3,7 @@ import { PublicRegistrationInitResponse } from 'interfaces/publicRegistration';
 
 export const registrationInitResponse: PublicRegistrationInitResponse = {
   exam_session: {
-    registrationType: 'FULL',
+    type: 'FULL',
     level_code: ExamLevel.PERUS,
     language_code: ExamLanguage.ENG,
     max_participants: 5,
