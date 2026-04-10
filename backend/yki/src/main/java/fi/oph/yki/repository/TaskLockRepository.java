96081719910f32fb3effeb14eb1dcb23622bae86
Miika von Bell <miika.vonbell@rebase.fi>
2026-04-10 14:00:55 +0300

    OPHKIOS-350: add task_lock monitoring to yki backend

---
+++ backend/yki/src/main/java/fi/oph/yki/repository/TaskLockRepository.java
@@ -0,0 +1,9 @@
+package fi.oph.yki.repository;
+
+import fi.oph.yki.model.TaskLock;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskLockRepository extends JpaRepository<TaskLock, String> {
+}
