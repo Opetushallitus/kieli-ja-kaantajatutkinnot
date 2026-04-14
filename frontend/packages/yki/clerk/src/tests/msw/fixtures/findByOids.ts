17f46f028ecaa2caeb734c1e7fc40f7f02f945b1
Jarkko Pesonen <435495+jrkkp@users.noreply.github.com>
2026-04-14 11:43:07 +0300

    YKI(Frontend) Ignore import error

--- frontend/packages/yki/clerk/src/tests/msw/fixtures/findByOids.ts
+++ frontend/packages/yki/clerk/src/tests/msw/fixtures/findByOids.ts
@@ -1,4 +1,5 @@
 import { FindByOidsOrganizationResponse } from 'interfaces/clerkOrganizerRegistry';
+// @ts-expect-error import mock data without type declarations from JS file
 import findByOids from 'tests/msw/fixtures/findByOidsData';
 
 export const findByOidsResponse: FindByOidsOrganizationResponse[] = findByOids;
