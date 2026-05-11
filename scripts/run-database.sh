511e7f0c8b1890e1c45e3e35b047c575a7013e47
Saku K <6057704+saku-koodari@users.noreply.github.com>
2026-05-11 17:11:20 +0300

    YKI(Backend): Use docker image to initialize database

--- scripts/run-database.sh
+++ scripts/run-database.sh
@@ -1,24 +1,22 @@
 #!/usr/bin/env bash
 
-cd "$KIOS/backend/yki/db" || {
-    echo "could find backend/yki/db" >&2
-    exit 1
-}
+(
+    cd "$KIOS" || {
+        echo "could navigate to \$KIOS - folder." >&2
+        exit 1
+    }
 
-# open Docker Desktop on Mac to ensure Docker daemon is running
-# on linux you can use your favorite service manager to run docker daemon
-KERNEL="$(uname -s)"
-if [[ $KERNEL == "Darwin" ]]; then
-    echo "Opening Docker Desktop to ensure daemon is running"
-    open -a "Docker" --background
-    sleep 5
-fi
+    # open Docker Desktop on Mac to ensure Docker daemon is running
+    # on linux you can use your favorite service manager to run docker daemon
+    KERNEL="$(uname -s)"
+    if [[ $KERNEL == "Darwin" ]]; then
+        echo "Opening Docker Desktop to ensure daemon is running"
+        open -a "Docker" --background
+        sleep 5
+    fi
 
-set -x
-
-# start (and try running) posgres/database
-docker build -t postgres-yki-init .
-docker run --name postgres-yki -p 5432:5432 -d postgres-yki-init
-docker start "$(docker ps -a | grep "postgres-yki" | awk '{ print $1 }')"
-docker ps -a
+    echo "Initializing the backend"
+    docker-compose -f docker-compose-yki.yml up -d yki-postgres
+    docker ps -a
+)
 
