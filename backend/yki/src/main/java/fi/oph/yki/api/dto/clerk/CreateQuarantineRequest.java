9ec11aa7d1373f63709829f03dbe7a6ab14d8988
Saku K <6057704+saku-koodari@users.noreply.github.com>
2026-05-11 17:24:13 +0300

    YKI(Backend): Sanitize the incoming request

--- backend/yki/src/main/java/fi/oph/yki/api/dto/clerk/CreateQuarantineRequest.java
+++ backend/yki/src/main/java/fi/oph/yki/api/dto/clerk/CreateQuarantineRequest.java
@@ -1,5 +1,6 @@
 package fi.oph.yki.api.dto.clerk;
 
+import fi.oph.yki.util.StringUtil;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -16,4 +17,14 @@ public record CreateQuarantineRequest(
   String ssn,
   String email,
   String phoneNumber
-) {}
+) {
+  public CreateQuarantineRequest {
+    languageCode = StringUtil.sanitize(languageCode);
+    firstName = StringUtil.sanitize(firstName);
+    lastName = StringUtil.sanitize(lastName);
+    diaryNumber = StringUtil.sanitize(diaryNumber);
+    ssn = StringUtil.sanitize(ssn);
+    email = StringUtil.sanitize(email);
+    phoneNumber = StringUtil.sanitize(phoneNumber);
+  }
+}
