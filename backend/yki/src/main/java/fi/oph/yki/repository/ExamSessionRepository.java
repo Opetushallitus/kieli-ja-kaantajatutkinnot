dc98c35338a833b95e3540faa5bd4fce0fc85fcb
Jarkko Pesonen <435495+jrkkp@users.noreply.github.com>
2026-05-21 15:15:33 +0300

    YKI(Backend) Add from date to exam session fetch [deploy]

--- backend/yki/src/main/java/fi/oph/yki/repository/ExamSessionRepository.java
+++ backend/yki/src/main/java/fi/oph/yki/repository/ExamSessionRepository.java
@@ -2,6 +2,7 @@ package fi.oph.yki.repository;
 
 import fi.oph.yki.model.ExamSession;
 import fi.oph.yki.model.Organizer;
+import java.time.LocalDate;
 import java.util.List;
 import org.springframework.data.jpa.repository.Query;
 
@@ -19,6 +20,16 @@ public interface ExamSessionRepository extends BaseRepository<ExamSession> {
     " WHERE es.organizer = :organizer"
   )
   List<ExamSession> findByOrganizer(final Organizer organizer);
+
+  @Query(
+    "SELECT DISTINCT es FROM ExamSession es" +
+    " LEFT JOIN FETCH es.registrations r" +
+    " LEFT JOIN FETCH r.freeRegistration" +
+    " LEFT JOIN FETCH r.evaluation" +
+    " LEFT JOIN FETCH es.examDate ed" +
+    " WHERE es.organizer = :organizer AND ed.examDate >= :from"
+  )
+  List<ExamSession> findByOrganizerAndExamDateFrom(final Organizer organizer, final LocalDate from);
 
   @Query("SELECT DISTINCT es FROM ExamSession es" + " LEFT JOIN FETCH es.locations r" + " WHERE es IN (:examSessions)")
   List<ExamSession> findByExamSessionsWithLocation(final List<ExamSession> examSessions);
