90b07f5a5ea1360b7190541bb59a019caf63adca
Kimmo Taskinen <kimmo.taskinen@knowit.fi>
2026-05-26 10:05:03 +0300

    YKI(Frontend): Updated script id generation [deploy]

--- frontend/packages/yki/clerk/src/VirkailijaRaamit.tsx
+++ frontend/packages/yki/clerk/src/VirkailijaRaamit.tsx
@@ -1,13 +1,24 @@
 import { useEffect } from 'react';
 
+const maxRunningId = 10000;
+let runningId = 0;
+const generateId = (prefix?: string) => {
+  const idPrefix = prefix ? prefix + '-' : '';
+  runningId = (runningId + 1) % maxRunningId;
+
+  return '' + idPrefix + Date.now().toString(36) + runningId.toString(36);
+};
+
 type VirkailijaRaamitProps = {
   scriptUrl: string;
 };
 
 export const VirkailijaRaamit = ({ scriptUrl }: VirkailijaRaamitProps) => {
   useEffect(() => {
+    // eslint-disable-next-line no-console
+    console.log('VirkailijaRaamit: useEffect called with scriptUrl', scriptUrl);
     const showRaamit = !!scriptUrl;
-    const raamitId = 'virkailija-raamit';
+    const raamitId = generateId('virkailijaRaamit');
     let scriptElement: HTMLScriptElement;
 
     if (showRaamit && !document.getElementById(raamitId)) {
