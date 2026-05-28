e026efd12386ac517068caeff29e078a77bc9956
Kimmo Taskinen <kimmo.taskinen@knowit.fi>
2026-05-28 08:30:20 +0300

    YKI(Frontend): Removed modify button from organizer view

--- frontend/packages/yki/clerk/src/components/clerkRegister/ClerkRegisterOrganizerDetails.tsx
+++ frontend/packages/yki/clerk/src/components/clerkRegister/ClerkRegisterOrganizerDetails.tsx
@@ -4,7 +4,6 @@ import dayjs from 'dayjs';
 import i18next from 'i18next';
 import { useCallback, useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
-import { CustomButton } from 'shared/components';
 import { APIResponseStatus } from 'shared/enums';
 import { DateUtils } from 'shared/utils';
 
@@ -246,20 +245,6 @@ export const ClerkRegisterOrganizerDetails = ({
           <Label>{t('listing.contentLabels.extraInfo')}</Label>
           <Text>{row.extra}</Text>
         </div>
-      </div>
-      <div
-        className="columns"
-        style={{ justifyContent: 'flex-end', gap: '1rem' }}
-      >
-        <CustomButton variant="outlined">
-          {t('listing.actionButtons.modify')}
-        </CustomButton>
-        <CustomButton
-          variant="contained"
-          onClick={() => setIsAddModalOpen(true)}
-        >
-          {t('listing.actionButtons.addExamSession')}
-        </CustomButton>
       </div>
       <H4>{t('listing.contentLabels.upcomingExamSessions')}</H4>
       <ListTable
