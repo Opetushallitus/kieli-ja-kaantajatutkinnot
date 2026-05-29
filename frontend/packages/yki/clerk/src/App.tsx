5b3d62dae83719a8b9bb3e5d8774075010945b66
Kimmo Taskinen <kimmo.taskinen@knowit.fi>
2026-05-29 13:09:46 +0300

    YKI(Frontend): Retry raamit load with nonce [deploy]

--- frontend/packages/yki/clerk/src/App.tsx
+++ frontend/packages/yki/clerk/src/App.tsx
@@ -4,7 +4,6 @@ import { StyleCacheProvider } from 'shared/components';
 import { initI18n } from 'configs/i18n';
 import { setupStore } from 'redux/store';
 import { AppRouter } from 'routers/AppRouter';
-import { VirkailijaRaamit } from 'VirkailijaRaamit';
 
 import 'styles/styles.scss';
 
@@ -13,16 +12,9 @@ initI18n();
 
 const store = setupStore();
 
-const virkailijaRaamitScriptUrl =
-  (window as Window & { Cypress?: unknown }).Cypress ||
-  'localhost' === window.location.hostname
-    ? ''
-    : `${window.location.origin}/virkailija-raamit/apply-raamit.js`;
-
 export const App = () => (
   <Provider store={store}>
     <StyleCacheProvider appName="yki">
-      <VirkailijaRaamit scriptUrl={virkailijaRaamitScriptUrl} />
       <AppRouter />
     </StyleCacheProvider>
   </Provider>
