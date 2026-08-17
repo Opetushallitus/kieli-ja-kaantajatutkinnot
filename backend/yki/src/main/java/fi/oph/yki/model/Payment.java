9b34a51ec6f9533e600790036d746244c57b423e
Jarkko Pesonen <435495+jrkkp@users.noreply.github.com>
2026-08-17 14:26:29 +0300

    YKI(Backend) Payment migration code

---
+++ backend/yki/src/main/java/fi/oph/yki/model/Payment.java
@@ -0,0 +1,59 @@
+package fi.oph.yki.model;
+
+import fi.oph.yki.model.type.PaymentStatus;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "payment")
+public class Payment extends BaseEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "payment_id", nullable = false)
+  private long id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "registration_id")
+  private Registration registration;
+
+  @Column(name = "amount", nullable = false)
+  private int amount;
+
+  @Column(name = "transaction_id")
+  private String transactionId;
+
+  @Column(name = "reference")
+  private String reference;
+
+  @Column(name = "merchant_reference")
+  private String merchantReference;
+
+  @Column(name = "payment_url")
+  private String paymentUrl;
+
+  @Enumerated(EnumType.STRING)
+  @Column(name = "payment_status")
+  private PaymentStatus paymentStatus;
+
+  @Column(name = "refunded_at")
+  private LocalDateTime refundedAt;
+
+  @Column(name = "paid_at")
+  private LocalDateTime paidAt;
+}
