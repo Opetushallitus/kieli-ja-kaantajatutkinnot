4824eadc1ff39e0d05b070224266ca498749b2a2
Miika von Bell <miika.vonbell@rebase.fi>
2026-09-01 14:11:10 +0300

    YKI(Backend): Add Participant and LoginLink JPA entities

---
+++ backend/yki/src/main/java/fi/oph/yki/model/Participant.java
@@ -0,0 +1,39 @@
+package fi.oph.yki.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
+import java.time.LocalDateTime;
+import java.util.Objects;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "participant")
+public class Participant {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id", nullable = false)
+  private long id;
+
+  @Column(name = "external_user_id", nullable = false, unique = true)
+  private String externalUserId;
+
+  @Column(name = "email")
+  private String email;
+
+  @Column(name = "created")
+  private LocalDateTime createdAt;
+
+  @Transient
+  public boolean isEmailAuth() {
+    return Objects.equals(externalUserId, email);
+  }
+}
