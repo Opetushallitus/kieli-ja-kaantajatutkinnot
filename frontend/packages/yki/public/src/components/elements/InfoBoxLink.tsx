9ae463009142518dfe2f88329167ddf7c110ac08
Kimmo Taskinen <kimmo.taskinen@knowit.fi>
2026-09-17 15:27:42 +0300

    YKI(Frontend): Updated Infobox content [deploy]

--- frontend/packages/yki/public/src/components/elements/InfoBoxLink.tsx
+++ frontend/packages/yki/public/src/components/elements/InfoBoxLink.tsx
@@ -2,13 +2,13 @@ import { WebLink } from 'shared/components';
 
 import { useCommonTranslation } from 'configs/i18n';
 
-export const PartialExamsLink = () => {
+export const InfoBoxLink = () => {
   const translateCommon = useCommonTranslation();
 
   return (
     <WebLink
-      href={translateCommon('partialExamsLink.url')}
-      label={translateCommon('partialExamsLink.label')}
+      href={translateCommon('infoBoxLink.url')}
+      label={translateCommon('infoBoxLink.label')}
     />
   );
 };
