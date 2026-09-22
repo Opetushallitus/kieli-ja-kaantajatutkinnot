711e2634993883cd28d8c9df5b4ced8e11e6c678
Saku K <6057704+saku-koodari@users.noreply.github.com>
2026-09-22 17:04:32 +0300

    YKI(Backend): Load public evaluation controllers only if the flag is enabled

--- backend/yki/src/main/java/fi/oph/yki/api/PublicEvaluationController.java
+++ backend/yki/src/main/java/fi/oph/yki/api/PublicEvaluationController.java
@@ -7,6 +7,7 @@ import fi.oph.yki.api.dto.PublicEvaluationPeriodDTO;
 import fi.oph.yki.api.dto.PublicEvaluationPeriodsResponseDTO;
 import fi.oph.yki.service.PublicEvaluationService;
 import jakarta.annotation.Resource;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/v2/api/public/evaluation", produces = APPLICATION_JSON_VALUE)
+@ConditionalOnProperty(name = "app.customer-portal.enabled", havingValue = "true")
 public class PublicEvaluationController {
 
   @Resource
--- backend/yki/src/test/java/fi/oph/yki/api/PublicEvaluationControllerTest.java
+++ backend/yki/src/test/java/fi/oph/yki/api/PublicEvaluationControllerTest.java
@@ -19,11 +19,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(PublicEvaluationController.class)
 @Import(ControllerExceptionAdvice.class)
+@TestPropertySource(properties = "app.customer-portal.enabled=true")
 @WithMockUser
 class PublicEvaluationControllerTest {
 
